9eb762a9b7511b758e59c2fbb3b36f9f4146fd63
Felix Wang <felix.wang@oocl.com>
2020-11-06 08:34:24 +0800

    No.5 - Use binary search, time complexity is O(log(min(m,n)))

--- src/november/MedianOfTwoSortedArrays.java
+++ src/november/MedianOfTwoSortedArrays.java
@@ -5,9 +5,49 @@ import java.util.List;
 
 public class MedianOfTwoSortedArrays {
     public static void main(String[] args) {
-        int[] nums1 = new int[]{0, 0};
-        int[] nums2 = new int[]{0, 0};
-        System.out.println(solution1(nums1, nums2));
+        int[] nums1 = new int[]{1, 3};
+        int[] nums2 = new int[]{4, 5};
+//        System.out.println(solution1(nums1, nums2));
+        System.out.println(solution2(nums1, nums2));
+    }
+
+    private static double solution2(int[] nums1, int[] nums2) {
+        double median;
+        if (nums1.length > nums2.length) {
+            int[] temp = nums1;
+            nums1 = nums2;
+            nums2 = temp;
+        }
+        int m = nums1.length;
+        int n = nums2.length;
+        int leftSize = (m + n + 1) / 2;
+        int left = 0;
+        int right = m;
+//        二分查找, 查找区间为[0,m], 满足条件 nums1[i-1] <= nums2[j] && nums2[j-1] <= nums1[i]
+//        循环终止条件为left == right, 此时left = right = i (第一个数组分割线右边的第一个元素下标)
+        while (left < right) {
+            int i = (left + right + 1) / 2;
+            int j = leftSize - i;
+            if (nums1[i - 1] > nums2[j]) {
+//                查询区间为[left, i-1]
+                right = i - 1;
+            } else {
+//                查找区间为[i, right]
+                left = i;
+            }
+        }
+        int i = left;
+        int j = leftSize - left;
+        int leftMax1 = i == 0 ? Integer.MIN_VALUE : nums1[i - 1];
+        int leftMax2 = j == 0 ? Integer.MIN_VALUE : nums2[j - 1];
+        int rightMin1 = i == m ? Integer.MAX_VALUE : nums1[i];
+        int rightMin2 = j == n ? Integer.MAX_VALUE : nums2[j];
+        if ((n + m) % 2 == 0) {
+            median = (double) (Math.max(leftMax1, leftMax2) + Math.min(rightMin1, rightMin2)) / 2;
+        } else {
+            median = Math.max(leftMax1, leftMax2);
+        }
+        return median;
     }
 
     private static double solution1(int[] nums1, int[] nums2) {
