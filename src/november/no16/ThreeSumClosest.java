345243c103b7f220cd02532e3f27da4371c45856
Felix Wang <felix.wang@oocl.com>
2020-11-24 19:15:26 +0800

    No.16 - Use sort and two pointers, time is O(n^2)

--- src/november/no16/ThreeSumClosest.java
+++ src/november/no16/ThreeSumClosest.java
@@ -4,14 +4,49 @@ import java.util.Arrays;
 
 public class ThreeSumClosest {
     public static void main(String[] args) {
-        int[] nums = new int[]{-6, -2, 0, 1, 4, 8, 15};
-        System.out.println(binarySearchClosest(nums, 0, -20));
-//        int[] nums = new int[]{-1, 2, 1, -4};
-//        int target = 1;
-//        System.out.println(threeSumClosest(nums, target));
+        int[] nums = new int[]{-3, -2, -5, 3, -4};
+        int target = -1;
+        System.out.println(threeSumClosest2(nums, target));
     }
 
-    private static int threeSumClosest(int[] nums, int target) {
+    private static int threeSumClosest2(int[] nums, int target) {
+//        use sort and two pointers, time is O(n^2)
+        if (nums == null || nums.length < 3) {
+            return 0;
+        }
+        int closest = 100000;
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length - 2; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+            int left = i + 1;
+            int right = nums.length - 1;
+            while (left < right) {
+                int sum = nums[i] + nums[left] + nums[right];
+                if (sum == target) {
+                    return target;
+                }
+                if (Math.abs(sum - target) < Math.abs(closest - target)) {
+                    closest = sum;
+                }
+                if (sum > target) {
+                    int curRight = right;
+                    while (left < right && nums[right] == nums[curRight]) {
+                        right--;
+                    }
+                } else {
+                    int curLeft = left;
+                    while (left < right && nums[left] == nums[curLeft]) {
+                        left++;
+                    }
+                }
+            }
+        }
+        return closest;
+    }
+
+    private static int threeSumClosest1(int[] nums, int target) {
         if (nums == null || nums.length < 3) {
             return 0;
         }
