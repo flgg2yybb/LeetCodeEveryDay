bdadf46fdb4183e80e45bae47df1a7d898be1f18
Felix Wang <felix.wang@oocl.com>
2020-11-24 16:16:00 +0800

    No.16 - Use brute force, time is O(n^3)

---
+++ src/november/no16/ThreeSumClosest.java
@@ -0,0 +1,83 @@
+package november.no16;
+
+import java.util.Arrays;
+
+public class ThreeSumClosest {
+    public static void main(String[] args) {
+        int[] nums = new int[]{-1, 2, 1, -4};
+        int target = 1;
+        System.out.println(threeSumClosest(nums, target));
+    }
+
+    private static int threeSumClosest(int[] nums, int target) {
+        if (nums == null || nums.length < 3) {
+            return 0;
+        }
+//        ascending sorting
+        Arrays.sort(nums);
+        int margin = Integer.MAX_VALUE;
+        int closest = 0;
+        for (int i = 0; i < nums.length - 2; i++) {
+            for (int j = i + 1; j < nums.length - 1; j++) {
+                int k = target - nums[i] - nums[j];
+                int closestK = linearSearch(nums, j + 1, k);
+//                int closestK = binarySearchClosest(nums, j + 1, k);
+                if (Math.abs(k - closestK) < margin) {
+                    margin = Math.abs(k - closestK);
+                    closest = nums[i] + nums[j] + closestK;
+                }
+            }
+
+        }
+        return closest;
+    }
+
+//    private static int binarySearchClosest(int[] nums, int start, int target) {
+////        nums is ascending sorting array
+//        int left = start;
+//        int right = nums.length - 1;
+//        int margin = Integer.MAX_VALUE;
+//        while (left < right) {
+//            int mid = (left + right) / 2;
+//
+//
+//        }
+//        return nums[left];
+//    }
+
+    private static int linearSearch(int[] nums, int start, int target) {
+        int margin = Integer.MAX_VALUE;
+        int closestTarget = 0;
+        for (int i = start; i < nums.length; i++) {
+            if (Math.abs(target - nums[i]) < margin) {
+                margin = Math.abs(target - nums[i]);
+                closestTarget = nums[i];
+            }
+        }
+        return closestTarget;
+    }
+}
+
+/*
+* 给定一个包括 n 个整数的数组 nums 和 一个目标值 target。找出 nums 中的三个整数，使得它们的和与 target 最接近。返回这三个数的和。假定每组输入只存在唯一答案。
+
+ 
+
+示例：
+
+输入：nums = [-1,2,1,-4], target = 1
+输出：2
+解释：与 target 最接近的和是 2 (-1 + 2 + 1 = 2) 。
+ 
+
+提示：
+
+3 <= nums.length <= 10^3
+-10^3 <= nums[i] <= 10^3
+-10^4 <= target <= 10^4
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/3sum-closest
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+* */
+
