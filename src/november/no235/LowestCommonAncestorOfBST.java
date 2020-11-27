139b4d2507e0dadd2f40bff2f5f18fa803199153
Felix Wang <felix.wang@oocl.com>
2020-11-27 08:34:12 +0800

    No.235 - Use BST property, if a node is lowest common ancestor, then p and q must not belong the left or right sub tree of current node. Time and space is O(n)

--- src/november/no235/LowestCommonAncestorOfBST.java
+++ src/november/no235/LowestCommonAncestorOfBST.java
@@ -13,9 +13,38 @@ public class LowestCommonAncestorOfBST {
                         new TreeNode(9)));
         TreeNode p = new TreeNode(2);
         TreeNode q = new TreeNode(5);
-        System.out.println(lowestCommonAncestor(root, p, q));
+        System.out.println(lowestCommonAncestor2(root, p, q));
     }
 
+/*
+* 我们从根节点开始遍历；
+
+如果当前节点的值大于 pp 和 qq 的值，说明 pp 和 qq 应该在当前节点的左子树，因此将当前节点移动到它的左子节点；
+
+如果当前节点的值小于 pp 和 qq 的值，说明 pp 和 qq 应该在当前节点的右子树，因此将当前节点移动到它的右子节点；
+
+如果当前节点的值不满足上述两条要求，那么说明当前节点就是「分岔点」。此时，pp 和 qq 要么在当前节点的不同的子树中，要么其中一个就是当前节点。
+
+作者：LeetCode-Solution
+链接：https://leetcode-cn.com/problems/lowest-common-ancestor-of-a-binary-search-tree/solution/er-cha-sou-suo-shu-de-zui-jin-gong-gong-zu-xian-26/
+来源：力扣（LeetCode）
+著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+* */
+
+    private static TreeNode lowestCommonAncestor2(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null) {
+            return root;
+        }
+        if (root.val > p.val && root.val > q.val) {
+            return lowestCommonAncestor(root.left, p, q);
+        }
+        if (root.val < p.val && root.val < q.val) {
+            return lowestCommonAncestor(root.right, p, q);
+        }
+        return root;
+    }
+
+    //    same solution with No.235
     private static TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
         if (root == null || root.val == p.val || root.val == q.val) {
             return root;
