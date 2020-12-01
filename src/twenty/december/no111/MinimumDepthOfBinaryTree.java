e09a3d66aa411b4963a6e2f117d7f6b1200ad3ff
Felix Wang <felix.wang@oocl.com>
2020-12-01 20:00:56 +0800

    No.111 - Simplify the recursion code

--- src/twenty/december/no111/MinimumDepthOfBinaryTree.java
+++ src/twenty/december/no111/MinimumDepthOfBinaryTree.java
@@ -14,6 +14,19 @@ public class MinimumDepthOfBinaryTree {
         if (root == null) {
             return 0;
         }
+        if (root.left == null) {
+            return 1 + minDepth(root.right);
+        }
+        if (root.right == null) {
+            return 1 + minDepth(root.left);
+        }
+        return Math.min(minDepth(root.left), minDepth(root.right)) + 1;
+    }
+
+    private static int myMinDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
         return getMinDepth(root, 1);
     }
 
