f024189b2794897262e86f1e41e7164f0d4e924d
Felix Wang <felix.wang@oocl.com>
2020-12-13 13:40:59 +0800

    No.62 - DP, time is O(mn), space is O(mn)

---
+++ src/twenty/december/no62/UniquePaths.java
@@ -0,0 +1,72 @@
+package twenty.december.no62;
+
+public class UniquePaths {
+    public static void main(String[] args) {
+        int m1 = 3, n1 = 7;
+        int m2 = 3, n2 = 2;
+        int m3 = 7, n3 = 3;
+        int m4 = 3, n4 = 3;
+        System.out.println(uniquePaths(m1, n1));
+        System.out.println(uniquePaths(m2, n2));
+        System.out.println(uniquePaths(m3, n3));
+        System.out.println(uniquePaths(m4, n4));
+    }
+
+    public static int uniquePaths(int m, int n) {
+//        dynamic programming, time is O(m * n), space is O(m * n)
+        int[][] dp = new int[m + 1][n + 1];
+        for (int row = m - 1; row >= 0; row--) {
+            for (int col = n - 1; col >= 0; col--) {
+                if (row == m - 1 && col == n - 1) {
+                    dp[row][col] = 1;
+                    continue;
+                }
+                dp[row][col] = dp[row + 1][col] + dp[row][col + 1];
+            }
+        }
+        return dp[0][0];
+    }
+}
+
+/*
+* 一个机器人位于一个 m x n 网格的左上角 （起始点在下图中标记为 “Start” ）。
+
+机器人每次只能向下或者向右移动一步。机器人试图达到网格的右下角（在下图中标记为 “Finish” ）。
+
+问总共有多少条不同的路径？
+
+ 
+
+示例 1：
+
+
+输入：m = 3, n = 7
+输出：28
+示例 2：
+
+输入：m = 3, n = 2
+输出：3
+解释：
+从左上角开始，总共有 3 条路径可以到达右下角。
+1. 向右 -> 向右 -> 向下
+2. 向右 -> 向下 -> 向右
+3. 向下 -> 向右 -> 向右
+示例 3：
+
+输入：m = 7, n = 3
+输出：28
+示例 4：
+
+输入：m = 3, n = 3
+输出：6
+ 
+
+提示：
+
+1 <= m, n <= 100
+题目数据保证答案小于等于 2 * 109
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/unique-paths
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+* */
