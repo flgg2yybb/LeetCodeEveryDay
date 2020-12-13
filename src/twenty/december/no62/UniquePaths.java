83952ea556c9dc82a7870932d15413fdea1da45f
Felix Wang <felix.wang@oocl.com>
2020-12-13 13:49:30 +0800

    No.62 - DP, reverse the status transfer direction

--- src/twenty/december/no62/UniquePaths.java
+++ src/twenty/december/no62/UniquePaths.java
@@ -6,10 +6,24 @@ public class UniquePaths {
         int m2 = 3, n2 = 2;
         int m3 = 7, n3 = 3;
         int m4 = 3, n4 = 3;
-        System.out.println(uniquePaths(m1, n1));
-        System.out.println(uniquePaths(m2, n2));
-        System.out.println(uniquePaths(m3, n3));
-        System.out.println(uniquePaths(m4, n4));
+        System.out.println(uniquePaths2(m1, n1));
+        System.out.println(uniquePaths2(m2, n2));
+        System.out.println(uniquePaths2(m3, n3));
+        System.out.println(uniquePaths2(m4, n4));
+    }
+
+    private static int uniquePaths2(int m, int n) {
+        int[][] dp = new int[m][n];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i == 0 || j == 0) {
+                    dp[i][j] = 1;
+                    continue;
+                }
+                dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+            }
+        }
+        return dp[m - 1][n - 1];
     }
 
     public static int uniquePaths(int m, int n) {
