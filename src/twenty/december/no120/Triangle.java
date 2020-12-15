92d6f4f0ab6d685a3d3dbfa0698ed1303a86be49
Felix Wang <felix.wang@oocl.com>
2020-12-15 18:27:56 +0800

    No.120 - Use DP, time is O(n), space is O(n)

---
+++ src/twenty/december/no120/Triangle.java
@@ -0,0 +1,66 @@
+package twenty.december.no120;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.IntStream;
+
+public class Triangle {
+
+    public static void main(String[] args) {
+        List<Integer> row1 = Arrays.asList(2);
+        List<Integer> row2 = Arrays.asList(3, 4);
+        List<Integer> row3 = Arrays.asList(6, 5, 7);
+        List<Integer> row4 = Arrays.asList(4, 1, 8, 3);
+        List<List<Integer>> triangle = Arrays.asList(row1, row2, row3, row4);
+        System.out.println(minimumTotal(triangle));
+    }
+
+    public static int minimumTotal(List<List<Integer>> triangle) {
+        if (triangle == null || triangle.size() == 0) {
+            return 0;
+        }
+        int[] dp = new int[triangle.size()];
+        dp[0] = triangle.get(0).get(0);
+        int[] rowMin = new int[triangle.size()];
+        for (int i = 1; i < triangle.size(); i++) {
+            List<Integer> rowList = triangle.get(i);
+            for (int j = 0; j < rowList.size(); j++) {
+                int first = j > 0 ? dp[j - 1] : Integer.MAX_VALUE;
+                int second = j < rowList.size() - 1 ? dp[j] : Integer.MAX_VALUE;
+                rowMin[j] = Math.min(first, second);
+            }
+            for (int j = 0; j < rowList.size(); j++) {
+                dp[j] = rowMin[j] + rowList.get(j);
+            }
+        }
+        return IntStream.of(dp).min().orElse(Integer.MAX_VALUE);
+    }
+}
+
+/**
+ * 给定一个三角形，找出自顶向下的最小路径和。每一步只能移动到下一行中相邻的结点上。
+ * <p>
+ * 相邻的结点 在这里指的是 下标 与 上一层结点下标 相同或者等于 上一层结点下标 + 1 的两个结点。
+ * <p>
+ * <p>
+ * <p>
+ * 例如，给定三角形：
+ * <p>
+ * [
+ * [2],
+ * [3,4],
+ * [6,5,7],
+ * [4,1,8,3]
+ * ]
+ * 自顶向下的最小路径和为 11（即，2 + 3 + 5 + 1 = 11）。
+ * <p>
+ * <p>
+ * <p>
+ * 说明：
+ * <p>
+ * 如果你可以只使用 O(n) 的额外空间（n 为三角形的总行数）来解决这个问题，那么你的算法会很加分。
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/triangle
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
