bb46ff7beb30218271d084cc9e24bf77c293b890
Felix Wang <felix.wang@oocl.com>
2020-12-16 08:45:59 +0800

    No.152 - Use DP, time is O(n^2), space is O(n^2), but exceed memory limit

---
+++ src/twenty/december/no152/MaximumProductSubarray.java
@@ -0,0 +1,65 @@
+package twenty.december.no152;
+
+public class MaximumProductSubarray {
+    public static void main(String[] args) {
+        int[] num1 = new int[]{2, 3, -2, 4};
+        int[] num2 = new int[]{-2, 0, -1};
+        int[] num3 = new int[]{2, 3, -2, -4};
+        System.out.println(maxProduct(num1));
+        System.out.println(maxProduct(num2));
+        System.out.println(maxProduct(num3));
+    }
+
+    private static int maxProduct(int[] nums) {
+//        DP, dp[i][j]为num数组中从 i下标的元素到 j下标的元素的乘积，则
+//        dp[i][i] = num[i], j = i
+//        dp[i][j] = dp[i][j - 1] * num[j], j > i
+        int[][] dp = new int[nums.length][nums.length];
+        int max = Integer.MIN_VALUE;
+        for (int i = 0; i < nums.length; i++) {
+            dp[i][i] = nums[i];
+            if (max < dp[i][i]) {
+                max = dp[i][i];
+            }
+        }
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                dp[i][j] = dp[i][j - 1] * nums[j];
+                if (max < dp[i][j]) {
+                    max = dp[i][j];
+                }
+            }
+        }
+        return max;
+    }
+
+    private static void disp(int[][] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
+
+/**
+ * 给你一个整数数组 nums ，请你找出数组中乘积最大的连续子数组（该子数组中至少包含一个数字），并返回该子数组所对应的乘积。
+ * <p>
+ *  
+ * <p>
+ * 示例 1:
+ * <p>
+ * 输入: [2,3,-2,4]
+ * 输出: 6
+ * 解释: 子数组 [2,3] 有最大乘积 6。
+ * 示例 2:
+ * <p>
+ * 输入: [-2,0,-1]
+ * 输出: 0
+ * 解释: 结果不能为 2, 因为 [-2,-1] 不是子数组。
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/maximum-product-subarray
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
