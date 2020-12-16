585c2e384851304670b2febb385f666117f33735
Felix Wang <felix.wang@oocl.com>
2020-12-16 18:54:05 +0800

    No.152 - Use DP, time is O(n), space is O(n)

--- src/twenty/december/no152/MaximumProductSubarray.java
+++ src/twenty/december/no152/MaximumProductSubarray.java
@@ -1,19 +1,39 @@
 package twenty.december.no152;
 
+import java.util.stream.IntStream;
+
 public class MaximumProductSubarray {
     public static void main(String[] args) {
         int[] num1 = new int[]{2, 3, -2, 4};
         int[] num2 = new int[]{-2, 0, -1};
         int[] num3 = new int[]{2, 3, -2, -4};
-        System.out.println(maxProduct(num1));
-        System.out.println(maxProduct(num2));
-        System.out.println(maxProduct(num3));
+        System.out.println(maxProduct2(num1));
+        System.out.println(maxProduct2(num2));
+        System.out.println(maxProduct2(num3));
+    }
+
+    private static int maxProduct2(int[] nums) {
+//        DP, fmax[i]为nums数组中从下标为0的元素到下标为i元素中的最大子序列乘积
+//            fmax[i]为nums数组中从下标为0的元素到下标为i元素中的最小子序列乘积
+//            fmax[i] = fmax{fmax[i - 1] * nums[i], fmin[i - 1] * nums[i], nums[i]}
+//            fmin[i] = fmin{fmax[i - 1] * nums[i], fmin[i - 1] * nums[i], nums[i]}
+        int[] fmax = new int[nums.length];
+        int[] fmin = new int[nums.length];
+        fmax[0] = nums[0];
+        fmin[0] = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            int element1 = fmax[i - 1] * nums[i];
+            int element2 = fmin[i - 1] * nums[i];
+            fmax[i] = Math.max(nums[i], Math.max(element1, element2));
+            fmin[i] = Math.min(nums[i], Math.min(element1, element2));
+        }
+        return IntStream.of(fmax).max().getAsInt();
     }
 
     private static int maxProduct(int[] nums) {
-//        DP, dp[i][j]为num数组中从 i下标的元素到 j下标的元素的乘积，则
-//        dp[i][i] = num[i], j = i
-//        dp[i][j] = dp[i][j - 1] * num[j], j > i
+//        DP, dp[i][j]为nums数组中从 i下标的元素到 j下标的元素的乘积，则
+//        dp[i][i] = nums[i], j = i
+//        dp[i][j] = dp[i][j - 1] * nums[j], j > i
         int[][] dp = new int[nums.length][nums.length];
         int max = Integer.MIN_VALUE;
         for (int i = 0; i < nums.length; i++) {
