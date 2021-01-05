97667ac157d230b7c57707286578067307fd6f71
Felix Wang <felix.wang@oocl.com>
2021-01-05 16:34:04 +0800

    No.21 - Recursion, time is O(n + m), space is O(n + m)

--- src/year2021/month1/no21/MergeTwoSortedLists.java
+++ src/year2021/month1/no21/MergeTwoSortedLists.java
@@ -10,10 +10,26 @@ public class MergeTwoSortedLists {
         ListNode node32 = new ListNode(4, new ListNode(6, new ListNode(8)));
         ListNode node41 = null;
         ListNode node42 = null;
-        disp(mergeTwoLists1(node11, node12));
-        disp(mergeTwoLists1(node21, node22));
-        disp(mergeTwoLists1(node31, node32));
-        disp(mergeTwoLists1(node41, node42));
+        disp(mergeTwoLists2(node11, node12));
+        disp(mergeTwoLists2(node21, node22));
+        disp(mergeTwoLists2(node31, node32));
+        disp(mergeTwoLists2(node41, node42));
+    }
+
+    private static ListNode mergeTwoLists2(ListNode l1, ListNode l2) {
+        if (l1 == null) {
+            return l2;
+        }
+        if (l2 == null) {
+            return l1;
+        }
+        if (l1.val < l2.val) {
+            l1.next = mergeTwoLists2(l1.next, l2);
+            return l1;
+        } else {
+            l2.next = mergeTwoLists2(l1, l2.next);
+            return l2;
+        }
     }
 
     private static ListNode mergeTwoLists1(ListNode l1, ListNode l2) {
