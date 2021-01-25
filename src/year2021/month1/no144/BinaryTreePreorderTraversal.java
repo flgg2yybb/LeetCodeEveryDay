62a4f407bb2147cc4767726e6d2886bcebd36b6d
Felix Wang <felix.wang@oocl.com>
2021-01-25 08:31:46 +0800

    No.144 - Pre-order Traversal, using dfs and stack, time is O(n), space is O(n)

---
+++ src/year2021/month1/no144/BinaryTreePreorderTraversal.java
@@ -0,0 +1,119 @@
+package year2021.month1.no144;
+
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.List;
+
+public class BinaryTreePreorderTraversal {
+    public static void main(String[] args) {
+        TreeNode root1 = new TreeNode(1, null, new TreeNode(2, new TreeNode(3), null));
+        TreeNode root2 = null;
+        TreeNode root3 = new TreeNode(1);
+        TreeNode root4 = new TreeNode(1, new TreeNode(2), null);
+        TreeNode root5 = new TreeNode(1, null, new TreeNode(2));
+        System.out.println(preorderTraversal1(root1));
+        System.out.println(preorderTraversal1(root2));
+        System.out.println(preorderTraversal1(root3));
+        System.out.println(preorderTraversal1(root4));
+        System.out.println(preorderTraversal1(root5));
+    }
+
+    private static List<Integer> preorderTraversal1(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        Deque<Object> stack = new LinkedList<>();
+        stack.push(root);
+        while (!stack.isEmpty()) {
+            Object pop = stack.pop();
+            if (pop == null) {
+                continue;
+            }
+            if (pop instanceof Integer) {
+                Integer value = (Integer) pop;
+                result.add(value);
+                continue;
+            }
+            TreeNode node = (TreeNode) pop;
+            stack.push(node.right);
+            stack.push(node.left);
+            stack.push(node.val);
+        }
+        return result;
+    }
+
+    public static List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        dfs(root, result);
+        return result;
+    }
+
+    private static void dfs(TreeNode root, List<Integer> result) {
+        if (root != null) {
+            result.add(root.val);
+            dfs(root.left, result);
+            dfs(root.right, result);
+        }
+    }
+}
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+/*
+* 给你二叉树的根节点 root ，返回它节点值的 前序 遍历。
+
+ 
+
+示例 1：
+
+
+输入：root = [1,null,2,3]
+输出：[1,2,3]
+示例 2：
+
+输入：root = []
+输出：[]
+示例 3：
+
+输入：root = [1]
+输出：[1]
+示例 4：
+
+
+输入：root = [1,2]
+输出：[1,2]
+示例 5：
+
+
+输入：root = [1,null,2]
+输出：[1,2]
+ 
+
+提示：
+
+树中节点数目在范围 [0, 100] 内
+-100 <= Node.val <= 100
+ 
+
+进阶：递归算法很简单，你可以通过迭代算法完成吗？
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/binary-tree-preorder-traversal
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+* */
