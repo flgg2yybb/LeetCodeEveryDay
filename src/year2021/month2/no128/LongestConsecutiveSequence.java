2396cc444a638af513fda4ff27d8866e999219e7
Felix Wang <felix.wang@oocl.com>
2021-02-02 08:37:55 +0800

    No.128 - Use sort, time is O(nlogn), space is O(1)

---
+++ src/year2021/month2/no128/LongestConsecutiveSequence.java
@@ -0,0 +1,69 @@
+package year2021.month2.no128;
+
+import java.util.Arrays;
+
+public class LongestConsecutiveSequence {
+    public static void main(String[] args) {
+        int[] nums1 = new int[]{100, 4, 200, 1, 3, 2};
+        int[] nums2 = new int[]{0, 3, 7, 2, 5, 8, 4, 6, 0, 1};
+        int[] nums3 = new int[]{1};
+        int[] nums4 = new int[0];
+        int[] nums5 = new int[]{0, 2, 1, 1};
+        System.out.println(longestConsecutive(nums1));  //4
+        System.out.println(longestConsecutive(nums2));  //9
+        System.out.println(longestConsecutive(nums3));  //1
+        System.out.println(longestConsecutive(nums4));  //0
+        System.out.println(longestConsecutive(nums5));  //3
+    }
+
+    public static int longestConsecutive(int[] nums) {
+        if (nums.length < 2) {
+            return nums.length;
+        }
+        Arrays.sort(nums);
+        int max = 0;
+        int count = 1;
+        for (int i = 0; i < nums.length - 1; i++) {
+            if (nums[i] == nums[i + 1]) {
+                continue;
+            }
+            if (nums[i] + 1 != nums[i + 1]) {
+                max = Math.max(max, count);
+                count = 1;
+                continue;
+            }
+            count++;
+        }
+        return Math.max(max, count);
+    }
+}
+
+/*
+* 给定一个未排序的整数数组 nums ，找出数字连续的最长序列（不要求序列元素在原数组中连续）的长度。
+
+ 
+
+进阶：你可以设计并实现时间复杂度为 O(n) 的解决方案吗？
+
+ 
+
+示例 1：
+
+输入：nums = [100,4,200,1,3,2]
+输出：4
+解释：最长数字连续序列是 [1, 2, 3, 4]。它的长度为 4。
+示例 2：
+
+输入：nums = [0,3,7,2,5,8,4,6,0,1]
+输出：9
+ 
+
+提示：
+
+0 <= nums.length <= 104
+-109 <= nums[i] <= 109
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/longest-consecutive-sequence
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+* */
