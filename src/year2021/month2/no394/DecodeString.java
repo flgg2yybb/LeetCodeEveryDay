9bc525da092ac2e16429c0be35ca22f038351d0a
Felix Wang <felix.wang@oocl.com>
2021-02-23 09:32:45 +0800

    No.394 - Recursion, time is O(n), space is O(n)

---
+++ src/year2021/month2/no394/DecodeString.java
@@ -0,0 +1,93 @@
+package year2021.month2.no394;
+
+public class DecodeString {
+    public static void main(String[] args) {
+        String s1 = "3[a]2[bc]";
+        String s2 = "3[a2[c]]";
+        String s3 = "2[abc]3[cd]ef";
+        String s4 = "abc3[cd]xyz";
+        String s5 = "abccdxyz";
+        String s6 = "10[leetcode]";
+        System.out.println(decodeString(s1));
+        System.out.println(decodeString(s2));
+        System.out.println(decodeString(s3));
+        System.out.println(decodeString(s4));
+        System.out.println(decodeString(s5));
+        System.out.println(decodeString(s6));
+    }
+
+    public static String decodeString(String s) {
+        StringBuilder sb = new StringBuilder();
+        char[] chars = s.toCharArray();
+        StringBuilder digit = new StringBuilder();
+        for (int i = 0; i < chars.length; i++) {
+            if (!Character.isDigit(chars[i])) {
+                sb.append(chars[i]);
+                continue;
+            }
+            while (Character.isDigit(chars[i])) {
+                digit.append(chars[i]);
+                i++;
+            }
+            int times = Integer.valueOf(digit.toString());
+            digit = new StringBuilder();
+            int start = i + 1;
+            int end = start;
+            int balance = 1;    //括号匹配，代表还需要多少个右括号才能匹配
+            StringBuilder temp = new StringBuilder();
+            while (end < chars.length && balance != 0) {
+                char c = chars[end];
+                if (c == '[') {
+                    balance++;
+                }
+                if (c == ']') {
+                    balance--;
+                }
+                if (balance != 0) {
+                    temp.append(chars[end]);
+                }
+                end++;
+            }
+            String subString = decodeString(new String(temp));
+            for (int j = 0; j < times; j++) {
+                sb.append(subString);
+            }
+            i = end - 1;
+        }
+        return sb.toString();
+    }
+
+}
+
+/*
+* 给定一个经过编码的字符串，返回它解码后的字符串。
+
+编码规则为: k[encoded_string]，表示其中方括号内部的 encoded_string 正好重复 k 次。注意 k 保证为正整数。
+
+你可以认为输入字符串总是有效的；输入字符串中没有额外的空格，且输入的方括号总是符合格式要求的。
+
+此外，你可以认为原始数据不包含数字，所有的数字只表示重复的次数 k ，例如不会出现像 3a 或 2[4] 的输入。
+
+ 
+
+示例 1：
+
+输入：s = "3[a]2[bc]"
+输出："aaabcbc"
+示例 2：
+
+输入：s = "3[a2[c]]"
+输出："accaccacc"
+示例 3：
+
+输入：s = "2[abc]3[cd]ef"
+输出："abcabccdcdcdef"
+示例 4：
+
+输入：s = "abc3[cd]xyz"
+输出："abccdcdcdxyz"
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/decode-string
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+* */
