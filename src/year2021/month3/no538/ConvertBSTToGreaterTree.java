5d387fe7bff50c1051cbc4661fc83f2e77c0bdb9
Felix Wang <felix.wang@oocl.com>
2021-03-08 08:41:51 +0800

    No.538 - DFS, time is O(n), space is O(n)

---
+++ src/year2021/month3/no538/ConvertBSTToGreaterTree.java
@@ -0,0 +1,119 @@
+package year2021.month3.no538;
+
+public class ConvertBSTToGreaterTree {
+    private static int temp = 0;
+
+    public static void main(String[] args) {
+        TreeNode root1 = new TreeNode(4,
+                new TreeNode(1,
+                        new TreeNode(0),
+                        new TreeNode(2,
+                                null,
+                                new TreeNode(3))),
+                new TreeNode(6,
+                        new TreeNode(5),
+                        new TreeNode(7,
+                                null,
+                                new TreeNode(8))));
+        TreeNode root2 = new TreeNode(0, null, new TreeNode(1));
+        TreeNode root3 = new TreeNode(1, new TreeNode(0), new TreeNode(2));
+        TreeNode root4 = new TreeNode(3,
+                new TreeNode(2, new TreeNode(1), null),
+                new TreeNode(4));
+        disp(convertBST(root1));
+        System.out.println();
+        disp(convertBST(root2));
+        System.out.println();
+        disp(convertBST(root3));
+        System.out.println();
+        disp(convertBST(root4));
+
+    }
+
+    public static TreeNode convertBST(TreeNode root) {
+        temp = 0;
+        dfs(root);
+        return root;
+    }
+
+    private static void dfs(TreeNode root) {
+        if (root != null) {
+            dfs(root.right);
+            temp += root.val;
+            root.val = temp;
+            dfs(root.left);
+        }
+    }
+
+    private static void disp(TreeNode root) {
+        if (root != null) {
+            disp(root.left);
+            System.out.print(root.val + " ");
+            disp(root.right);
+        }
+    }
+}
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+/*
+ * 给出二叉 搜索 树的根节点，该树的节点值各不相同，请你将其转换为累加树（Greater Sum Tree），使每个节点 node 的新值等于原树中大于或等于 node.val 的值之和。
+
+提醒一下，二叉搜索树满足下列约束条件：
+
+节点的左子树仅包含键 小于 节点键的节点。
+节点的右子树仅包含键 大于 节点键的节点。
+左右子树也必须是二叉搜索树。
+注意：本题和 1038: https://leetcode-cn.com/problems/binary-search-tree-to-greater-sum-tree/ 相同
+
+ 
+
+示例 1：
+
+
+
+输入：[4,1,6,0,2,5,7,null,null,null,3,null,null,null,8]
+输出：[30,36,21,36,35,26,15,null,null,null,33,null,null,null,8]
+示例 2：
+
+输入：root = [0,null,1]
+输出：[1,null,1]
+示例 3：
+
+输入：root = [1,0,2]
+输出：[3,3,2]
+示例 4：
+
+输入：root = [3,2,4,1]
+输出：[7,9,4,10]
+ 
+
+提示：
+* 
+
+树中的节点数介于 0 和 104 之间。
+每个节点的值介于 -104 和 104 之间。
+树中的所有值 互不相同 。
+给定的树为二叉搜索树。
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/convert-bst-to-greater-tree
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+* */
