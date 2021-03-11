d9d7e8340b916367e6a6e82491c7179ef6b8e536
Felix Wang <felix.wang@oocl.com>
2021-03-11 08:48:51 +0800

    No.617 - DFS, time is O(min(n,m)), space is O(min(n,m)), n and m is the amount of two binary trees' node

---
+++ src/year2021/month3/no617/MergeTwoBinaryTrees.java
@@ -0,0 +1,75 @@
+package year2021.month3.no617;
+
+public class MergeTwoBinaryTrees {
+    public static void main(String[] args) {
+        TreeNode t1 = new TreeNode(1,
+                new TreeNode(3, new TreeNode(5), null),
+                new TreeNode(2));
+        TreeNode t2 = new TreeNode(2,
+                new TreeNode(1, null, new TreeNode(4)),
+                new TreeNode(3, null, new TreeNode(7)));
+        dfs(mergeTrees(t1, t2));
+    }
+
+    public static TreeNode mergeTrees(TreeNode t1, TreeNode t2) {
+        if (t1 == null || t2 == null) {
+            return t1 == null ? t2 : t1;
+        }
+        TreeNode merge = new TreeNode(t1.val + t2.val);
+        merge.left = mergeTrees(t1.left, t2.left);
+        merge.right = mergeTrees(t1.right, t2.right);
+        return merge;
+    }
+
+    private static void dfs(TreeNode root) {
+        if (root != null) {
+            System.out.print(root.val + " ");
+            dfs(root.left);
+            dfs(root.right);
+        }
+    }
+}
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x) {
+        val = x;
+    }
+
+    public TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+/*
+* 给定两个二叉树，想象当你将它们中的一个覆盖到另一个上时，两个二叉树的一些节点便会重叠。
+
+你需要将他们合并为一个新的二叉树。合并的规则是如果两个节点重叠，那么将他们的值相加作为节点合并后的新值，否则不为  NULL 的节点将直接作为新二叉树的节点。
+
+示例  1:
+
+输入: 
+	Tree 1                     Tree 2                  
+          1                         2                             
+         / \                       / \                            
+        3   2                     1   3                        
+       /                           \   \                      
+      5                             4   7                  
+输出: 
+合并后的树:
+	     3
+	    / \
+	   4   5
+	  / \   \ 
+	 5   4   7
+注意:  合并必须从两个树的根节点开始。
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/merge-two-binary-trees
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+* */
