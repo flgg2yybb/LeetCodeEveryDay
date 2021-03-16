41c0dde504c3f8ea5b470f1657882cf8d1212dcc
Felix Wang <felix.wang@oocl.com>
2021-03-16 08:23:07 +0800

    No.217 - Set, time is O(n), space is O(n)

---
+++ src/year2021/month3/no217/ContainsDuplicate.java
@@ -0,0 +1,51 @@
+package year2021.month3.no217;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class ContainsDuplicate {
+    public static void main(String[] args) {
+        int[] nums1 = {1, 2, 3, 1};
+        int[] nums2 = {1, 2, 3, 4};
+        int[] nums3 = {1, 1, 1, 3, 3, 4, 3, 2, 4, 2};
+        System.out.println(containsDuplicate(nums1));
+        System.out.println(containsDuplicate(nums2));
+        System.out.println(containsDuplicate(nums3));
+    }
+
+    public static boolean containsDuplicate(int[] nums) {
+        Set<Integer> set = new HashSet<>();
+        for (int num : nums) {
+            if (set.contains(num)) {
+                return true;
+            }
+            set.add(num);
+        }
+        return false;
+    }
+}
+
+/*
+* 给定一个整数数组，判断是否存在重复元素。
+
+如果存在一值在数组中出现至少两次，函数返回 true 。如果数组中每个元素都不相同，则返回 false 。
+
+ 
+
+示例 1:
+
+输入: [1,2,3,1]
+输出: true
+示例 2:
+
+输入: [1,2,3,4]
+输出: false
+示例 3:
+
+输入: [1,1,1,3,3,4,3,2,4,2]
+输出: true
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/contains-duplicate
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+* */
