25590f991c4e1f1f070be98553e65c48b0d3fd15
Felix Wang <felix.wang@oocl.com>
2021-03-25 19:12:29 +0800

    No.21 - Recursion, time is O(min(n,m)), space is O(min(n,m))

--- src/year2021/month3/no21/MergeTwoSortedLists.java
+++ src/year2021/month3/no21/MergeTwoSortedLists.java
@@ -16,7 +16,22 @@ public class MergeTwoSortedLists {
         disp(mergeTwoLists(l41, l42));
     }
 
-    public static ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+    private static ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        if (l1 == null || l2 == null) {
+            return l1 == null ? l2 : l1;
+        }
+        ListNode res;
+        if (l1.val <= l2.val) {
+            res = l1;
+            res.next = mergeTwoLists(l1.next, l2);
+        } else {
+            res = l2;
+            res.next = mergeTwoLists(l1, l2.next);
+        }
+        return res;
+    }
+
+    public static ListNode mergeTwoLists1(ListNode l1, ListNode l2) {
         if (l1 == null || l2 == null) {
             return l1 == null ? l2 : l1;
         }
