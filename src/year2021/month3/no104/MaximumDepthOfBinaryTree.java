c5152baf1cd95ffb4bbf4b51b21fa8a497ff68b1
Felix Wang <felix.wang@oocl.com>
2021-03-26 08:35:06 +0800

    No.104 - DFS, time is O(n), space is O(n)

---
+++ src/year2021/month3/no104/MaximumDepthOfBinaryTree.java
@@ -0,0 +1,64 @@
+package year2021.month3.no104;
+
+public class MaximumDepthOfBinaryTree {
+    public static void main(String[] args) {
+        TreeNode root1 = null;
+        TreeNode root2 = new TreeNode(3,
+                new TreeNode(9),
+                new TreeNode(20,
+                        new TreeNode(15),
+                        new TreeNode(7)));
+        System.out.println(maxDepth(root1));
+        System.out.println(maxDepth(root2));
+    }
+
+    public static int maxDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int leftDepth = maxDepth(root.left);
+        int rightDepth = maxDepth(root.right);
+        return 1 + Math.max(leftDepth, rightDepth);
+    }
+}
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+/*
+* 给定一个二叉树，找出其最大深度。
+
+二叉树的深度为根节点到最远叶子节点的最长路径上的节点数。
+
+说明:  叶子节点是指没有子节点的节点。
+
+示例：
+给定二叉树 [3,9,20,null,null,15,7]，
+
+    3
+   / \
+  9  20
+    /  \
+   15   7
+返回它的最大深度  3 。
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/maximum-depth-of-binary-tree
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+* */
