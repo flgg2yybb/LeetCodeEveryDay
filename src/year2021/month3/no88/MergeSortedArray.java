da8e5c2aea180f0ff7751c45a3972f1e6cfb17c0
Felix <742863722@qq.com>
2021-03-28 09:52:16 +0800

    No.88 - Merge Sorted Array, time is O(m+n), space is O(m+n)

---
+++ src/year2021/month3/no88/MergeSortedArray.java
@@ -0,0 +1,66 @@
+package year2021.month3.no88;
+
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+public class MergeSortedArray {
+    public static void main(String[] args) {
+        int[] nums1 = {1, 2, 3, 0, 0, 0};
+        int m1 = 3;
+        int[] nums2 = {2, 5, 6};
+        int n1 = 3;
+        merge(nums1, m1, nums2, n1);
+        System.out.println(Arrays.stream(nums1).boxed().collect(Collectors.toList()));
+    }
+
+    public static void merge(int[] nums1, int m, int[] nums2, int n) {
+        int[] res = new int[m + n];
+        int k = 0;
+        int i = 0;
+        int j = 0;
+        while (i < m && j < n) {
+            if (nums1[i] <= nums2[j]) {
+                res[k++] = nums1[i++];
+            } else {
+                res[k++] = nums2[j++];
+            }
+        }
+        while (i < m) {
+            res[k++] = nums1[i++];
+        }
+        while (j < n) {
+            res[k++] = nums2[j++];
+        }
+        System.arraycopy(res, 0, nums1, 0, res.length);
+    }
+}
+
+/*
+ * 给你两个有序整数数组 nums1 和 nums2，请你将 nums2 合并到 nums1 中，使 nums1 成为一个有序数组。
+
+初始化 nums1 和 nums2 的元素数量分别为 m 和 n 。你可以假设 nums1 的空间大小等于 m + n，这样它就有足够的空间保存来自 nums2 的元素。
+
+ 
+
+示例 1：
+
+输入：nums1 = [1,2,3,0,0,0], m = 3, nums2 = [2,5,6], n = 3
+输出：[1,2,2,3,5,6]
+示例 2：
+
+输入：nums1 = [1], m = 1, nums2 = [], n = 0
+输出：[1]
+ 
+
+提示：
+
+nums1.length == m + n
+nums2.length == n
+0 <= m, n <= 200
+1 <= m + n <= 200
+-109 <= nums1[i], nums2[i] <= 109
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/merge-sorted-array
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+* */
