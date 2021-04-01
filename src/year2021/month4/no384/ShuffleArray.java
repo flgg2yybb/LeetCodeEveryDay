a7c7ca5385cf3fdd8ca1e4f098b3b66538dc126a
Felix <742863722@qq.com>
2021-04-01 17:16:23 +0800

    No.384 - Shuffle, time is O(n^2), space is O(n)

---
+++ src/year2021/month4/no384/ShuffleArray.java
@@ -0,0 +1,87 @@
+package year2021.month4.no384;
+
+import java.util.List;
+import java.util.Random;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class ShuffleArray {
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 3};
+        Solution obj = new Solution(nums);
+        disp(obj.reset());
+        disp(obj.shuffle());
+    }
+
+    private static void disp(int[] nums) {
+        System.out.println(IntStream.of(nums).boxed().collect(Collectors.toList()));
+    }
+}
+
+class Solution {
+
+    private final int[] nums;
+
+    public Solution(int[] nums) {
+        this.nums = nums;
+    }
+
+    /**
+     * Resets the array to its original configuration and return it.
+     */
+    public int[] reset() {
+        return this.nums;
+    }
+
+    /**
+     * Returns a random shuffling of the array.
+     */
+    public int[] shuffle() {
+        List<Integer> list = IntStream.of(this.nums).boxed().collect(Collectors.toList());
+        int[] res = new int[this.nums.length];
+        int pos = 0;
+        Random random = new Random();
+        while (!list.isEmpty()) {
+            int randomIndex = random.nextInt(list.size());
+            res[pos++] = list.remove(randomIndex);
+        }
+        return res;
+    }
+}
+
+/*
+ * 给你一个整数数组 nums ，设计算法来打乱一个没有重复元素的数组。
+
+实现 Solution class:
+
+Solution(int[] nums) 使用整数数组 nums 初始化对象
+int[] reset() 重设数组到它的初始状态并返回
+int[] shuffle() 返回数组随机打乱后的结果
+ 
+
+示例：
+
+输入
+["Solution", "shuffle", "reset", "shuffle"]
+[[[1, 2, 3]], [], [], []]
+输出
+[null, [3, 1, 2], [1, 2, 3], [1, 3, 2]]
+
+解释
+Solution solution = new Solution([1, 2, 3]);
+solution.shuffle();    // 打乱数组 [1,2,3] 并返回结果。任何 [1,2,3]的排列返回的概率应该相同。例如，返回 [3, 1, 2]
+solution.reset();      // 重设数组到它的初始状态 [1, 2, 3] 。返回 [1, 2, 3]
+solution.shuffle();    // 随机返回数组 [1, 2, 3] 打乱后的结果。例如，返回 [1, 3, 2]
+ 
+
+提示：
+
+1 <= nums.length <= 200
+-106 <= nums[i] <= 106
+nums 中的所有元素都是 唯一的
+最多可以调用 5 * 104 次 reset 和 shuffle
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/shuffle-an-array
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+* */
