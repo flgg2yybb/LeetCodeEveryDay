4bb3675061e52e3f350d46ce403a434fe24a7f7b
Felix <742863722@qq.com>
2021-04-03 10:37:17 +0800

    No.155 - Monotonous Stack(单调栈), time is O(1), space is O(n)

---
+++ src/year2021/month4/no155/TestMinStack.java
@@ -0,0 +1,90 @@
+package year2021.month4.no155;
+
+import java.util.Deque;
+import java.util.LinkedList;
+
+public class TestMinStack {
+    public static void main(String[] args) {
+        MinStack minStack = new MinStack();
+        minStack.push(-2);
+        minStack.push(0);
+        minStack.push(-3);
+        System.out.println(minStack.getMin());      //-3
+        minStack.pop();
+        System.out.println(minStack.top());         //0
+        System.out.println(minStack.getMin());      //-2
+    }
+}
+
+class MinStack {
+
+    private final Deque<Integer> stack;
+    private final Deque<Integer> minStack;      //单调不增栈
+
+    public MinStack() {
+        stack = new LinkedList<>();
+        minStack = new LinkedList<>();
+    }
+
+    public void push(int val) {
+        stack.push(val);
+        if (minStack.isEmpty() || minStack.peek() >= val) {
+            minStack.push(val);
+        }
+    }
+
+    public void pop() {
+        Integer pop = stack.pop();
+        if (pop.equals(minStack.peek())) {
+            minStack.pop();
+        }
+    }
+
+    public int top() {
+        assert !stack.isEmpty();
+        return stack.peek();
+    }
+
+    public int getMin() {
+        assert !minStack.isEmpty();
+        return minStack.peek();
+    }
+}
+
+/*
+ * 设计一个支持 push ，pop ，top 操作，并能在常数时间内检索到最小元素的栈。
+
+push(x) —— 将元素 x 推入栈中。
+pop() —— 删除栈顶的元素。
+top() —— 获取栈顶元素。
+getMin() —— 检索栈中的最小元素。
+ 
+
+示例:
+
+输入：
+["MinStack","push","push","push","getMin","pop","top","getMin"]
+[[],[-2],[0],[-3],[],[],[],[]]
+
+输出：
+[null,null,null,null,-3,null,0,-2]
+
+解释：
+MinStack minStack = new MinStack();
+minStack.push(-2);
+minStack.push(0);
+minStack.push(-3);
+minStack.getMin();   --> 返回 -3.
+minStack.pop();
+minStack.top();      --> 返回 0.
+minStack.getMin();   --> 返回 -2.
+ 
+
+提示：
+
+pop、top 和 getMin 操作总是在 非空栈 上调用。
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/min-stack
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+* */
