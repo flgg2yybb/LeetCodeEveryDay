b0c72585ce6204a1c1540e8ec5d4e47aecaf9832
Felix Wang <felix.wang@oocl.com>
2021-04-20 08:58:37 +0800

    No.200 - DFS, time is O(nm), space is O(nm)

---
+++ src/year2021/month4/no200/NumberOfIslands.java
@@ -0,0 +1,96 @@
+package year2021.month4.no200;
+
+public class NumberOfIslands {
+
+    public static void main(String[] args) {
+        char[][] grid1 = {
+                {'1', '1', '1', '1', '0'},
+                {'1', '1', '0', '1', '0'},
+                {'1', '1', '0', '0', '0'},
+                {'0', '0', '0', '0', '0'}
+        };
+        char[][] grid2 = {
+                {'1', '1', '0', '0', '0'},
+                {'1', '1', '0', '0', '0'},
+                {'0', '0', '1', '0', '0'},
+                {'0', '0', '0', '1', '1'}
+        };
+        System.out.println(numIslands(grid1));
+        System.out.println(numIslands(grid2));
+    }
+
+    public static int numIslands(char[][] grid) {
+        //dfs
+        boolean[][] visited = new boolean[grid.length][grid[0].length];
+        int count = 0;
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                if (grid[i][j] == '1' && !visited[i][j]) {
+                    dfs(grid, visited, i, j);
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+
+    private static void dfs(char[][] grid, boolean[][] visited, int row, int col) {
+        visited[row][col] = true;
+        int[][] directions = {{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
+        for (int[] direction : directions) {
+            int nextRow = row + direction[0];
+            int nextCol = col + direction[1];
+            if (legalIndex(grid, nextRow, nextCol)
+                    && grid[nextRow][nextCol] == '1'
+                    && !visited[nextRow][nextCol]) {
+                dfs(grid, visited, nextRow, nextCol);
+            }
+        }
+    }
+
+    private static boolean legalIndex(char[][] grid, int nextRow, int nextCol) {
+        return nextRow >= 0 && nextRow < grid.length && nextCol >= 0 && nextCol < grid[0].length;
+    }
+
+}
+
+/*
+* 给你一个由 '1'（陆地）和 '0'（水）组成的的二维网格，请你计算网格中岛屿的数量。
+
+岛屿总是被水包围，并且每座岛屿只能由水平方向和/或竖直方向上相邻的陆地连接形成。
+
+此外，你可以假设该网格的四条边均被水包围。
+
+ 
+
+示例 1：
+
+输入：grid = [
+  ['1','1','1','1','0'],
+  ['1','1','0','1','0'],
+  ['1','1','0','0','0'],
+  ['0','0','0','0','0']
+]
+输出：1
+示例 2：
+
+输入：grid = [
+  ['1','1','0','0','0'],
+  ['1','1','0','0','0'],
+  ['0','0','1','0','0'],
+  ['0','0','0','1','1']
+]
+输出：3
+ 
+
+提示：
+
+m == grid.length
+n == grid[i].length
+1 <= m, n <= 300
+grid[i][j] 的值为 '0' 或 '1'
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/number-of-islands
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+* */
