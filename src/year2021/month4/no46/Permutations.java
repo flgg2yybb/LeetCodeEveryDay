a4979783115a1de54c7572a4a1dfbc0381065814
Felix Wang <felix.wang@oocl.com>
2021-04-22 08:45:51 +0800

    No.46 - Backtrack & Permutation, time is O(n * n!), space is O(n)

---
+++ src/year2021/month4/no46/Permutations.java
@@ -0,0 +1,61 @@
+package year2021.month4.no46;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Permutations {
+
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 3};
+        System.out.println(permute(nums));
+    }
+
+    public static List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        backtrack(result, nums, 0);
+        return result;
+    }
+
+    private static void backtrack(List<List<Integer>> result, int[] nums, int pos) {
+        if (pos == nums.length) {
+            result.add(Arrays.stream(nums).boxed().collect(Collectors.toList()));
+            return;
+        }
+        for (int i = pos; i < nums.length; i++) {
+            swap(nums, pos, i);
+            backtrack(result, nums, pos + 1);
+            swap(nums, pos, i);
+        }
+    }
+
+    private static void swap(int[] nums, int x, int y) {
+        int temp = nums[x];
+        nums[x] = nums[y];
+        nums[y] = temp;
+    }
+
+}
+
+/*
+* 给定一个 没有重复 数字的序列，返回其所有可能的全排列。
+
+示例:
+
+输入: [1,2,3]
+输出:
+[
+  [1,2,3],
+  [1,3,2],
+  [2,1,3],
+  [2,3,1],
+  [3,1,2],
+  [3,2,1]
+]
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/permutations
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+* */
