01568e5db8a9bbe26984473887f9fccb52bb1d1c
Felix Wang <felix.wang@oocl.com>
2021-05-05 16:31:34 +0800

    No.202 - HashSet, time is O(logn), space is O(logn)

---
+++ src/year2021/month5/no202/HappyNumber.java
@@ -0,0 +1,70 @@
+package year2021.month5.no202;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class HappyNumber {
+
+    public static void main(String[] args) {
+        int n1 = 19;
+        int n2 = 2;
+        System.out.println(isHappy(n1));
+        System.out.println(isHappy(n2));
+    }
+
+    public static boolean isHappy(int n) {
+        Set<Integer> set = new HashSet<>();
+        while (n != 1 && !set.contains(n)) {
+            set.add(n);
+            n = getNext(n);
+        }
+        return n == 1;
+    }
+
+    private static int getNext(int n) {
+        int sum = 0;
+        while (n > 0) {
+            int temp = n % 10;
+            sum += temp * temp;
+            n /= 10;
+        }
+        return sum;
+    }
+
+}
+
+/*
+* 编写一个算法来判断一个数 n 是不是快乐数。
+
+「快乐数」定义为：
+
+对于一个正整数，每一次将该数替换为它每个位置上的数字的平方和。
+然后重复这个过程直到这个数变为 1，也可能是 无限循环 但始终变不到 1。
+如果 可以变为  1，那么这个数就是快乐数。
+如果 n 是快乐数就返回 true ；不是，则返回 false 。
+
+ 
+
+示例 1：
+
+输入：19
+输出：true
+解释：
+12 + 92 = 82
+82 + 22 = 68
+62 + 82 = 100
+12 + 02 + 02 = 1
+示例 2：
+
+输入：n = 2
+输出：false
+ 
+
+提示：
+
+1 <= n <= 231 - 1
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/happy-number
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+* */
