dc47863ae285845c083c2298795d9b182b195a8a
Felix Wang <felix.wang@oocl.com>
2021-05-13 12:22:46 +0800

    No.557 - String Reverse, time is O(n), space is O(n)

---
+++ src/year2021/month5/no557/ReverseWordsInStringIII.java
@@ -0,0 +1,53 @@
+package year2021.month5.no557;
+
+public class ReverseWordsInStringIII {
+
+    public static void main(String[] args) {
+        String s = "Let's take LeetCode contest";
+        System.out.println(reverseWords(s));
+    }
+
+    public static String reverseWords(String s) {
+        String[] strings = s.split(" ");
+        StringBuilder ans = new StringBuilder();
+        for (String str : strings) {
+            ans.append(reverse(str)).append(" ");
+        }
+        ans.deleteCharAt(ans.length() - 1);
+        return ans.toString();
+    }
+
+    private static String reverse(String s) {
+        char[] chars = s.toCharArray();
+        int left = 0;
+        int right = chars.length - 1;
+        while (left < right) {
+            char temp = chars[left];
+            chars[left] = chars[right];
+            chars[right] = temp;
+            left++;
+            right--;
+        }
+        return new String(chars);
+    }
+
+}
+
+/*
+给定一个字符串，你需要反转字符串中每个单词的字符顺序，同时仍保留空格和单词的初始顺序。
+
+ 
+
+示例：
+
+输入："Let's take LeetCode contest"
+输出："s'teL ekat edoCteeL tsetnoc"
+ 
+
+提示：
+
+在字符串中，每个单词由单个空格分隔，并且字符串中不会有任何额外的空格。
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/reverse-words-in-a-string-iii
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。*/
