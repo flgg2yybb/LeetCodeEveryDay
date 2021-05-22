7ea596029e5674433d8e02d0b9f559049b2307b9
Felix Wang <felix.wang@oocl.com>
2021-05-22 11:29:09 +0800

    No.89 - Gray Code - Mirror Reflection, time is O(2^n), space is O(2^n)

---
+++ src/year2021/month5/no89/GrayCode.java
@@ -0,0 +1,68 @@
+package year2021.month5.no89;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GrayCode {
+
+    public static void main(String[] args) {
+        int n1 = 2;
+        int n2 = 0;
+        System.out.println(grayCode(n1));
+        System.out.println(grayCode(n2));
+    }
+
+    public static List<Integer> grayCode(int n) {
+//        Attached the solution link: https://leetcode-cn.com/problems/gray-code/solution/gray-code-jing-xiang-fan-she-fa-by-jyd/
+        List<Integer> ans = new ArrayList<>();
+        ans.add(0);
+        int mask = 1;
+        for (int i = 0; i < n; i++) {
+            for (int j = ans.size() - 1; j >= 0; j--) {
+                ans.add(mask + ans.get(j));
+            }
+            mask <<= 1;
+        }
+        return ans;
+    }
+
+}
+
+/*
+* 格雷编码是一个二进制数字系统，在该系统中，两个连续的数值仅有一个位数的差异。
+
+给定一个代表编码总位数的非负整数 n，打印其格雷编码序列。即使有多个不同答案，你也只需要返回其中一种。
+
+格雷编码序列必须以 0 开头。
+
+ 
+
+示例 1:
+
+输入: 2
+输出: [0,1,3,2]
+解释:
+00 - 0
+01 - 1
+11 - 3
+10 - 2
+
+对于给定的 n，其格雷编码序列并不唯一。
+例如，[0,2,3,1] 也是一个有效的格雷编码序列。
+
+00 - 0
+10 - 2
+11 - 3
+01 - 1
+示例 2:
+
+输入: 0
+输出: [0]
+解释: 我们定义格雷编码序列必须以 0 开头。
+     给定编码总位数为 n 的格雷编码序列，其长度为 2n。当 n = 0 时，长度为 20 = 1。
+     因此，当 n = 0 时，其格雷编码序列为 [0]。
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/gray-code
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+* */
