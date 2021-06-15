98413be06228d9ed6da3923d971ad95dd896b420
Felix Wang <felix.wang@oocl.com>
2021-06-15 19:33:42 +0800

    No.42 - Left & Right Bound calculation, time is O(n), space is O(n)

---
+++ src/year2021/month6/no42/TrappingRainWater.java
@@ -0,0 +1,70 @@
+package year2021.month6.no42;
+
+public class TrappingRainWater {
+
+    public static void main(String[] args) {
+        int[] height1 = {0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1};
+        int[] height2 = {4, 2, 0, 3, 2, 5};
+        System.out.println(trap(height1));
+        System.out.println(trap(height2));
+    }
+
+    public static int trap(int[] height) {
+        /*
+         * 分别计算每个位置上左右的最大边界
+         * 若左右最大边界中较小的那个比当前位置的高度高
+         * 表示当前位置是个凹槽，可以接雨水
+         * 则用较小的边界高度减去当前位置高度即可当前位置接到的雨水量
+         * time is O(n), space is O(n)
+         * */
+        if (height.length < 3) {
+            return 0;
+        }
+        int[] leftBound = new int[height.length];
+        leftBound[0] = height[0];
+        for (int i = 1; i < height.length; i++) {
+            leftBound[i] = Math.max(leftBound[i - 1], height[i]);
+        }
+        int[] rightBound = new int[height.length];
+        rightBound[height.length - 1] = height[height.length - 1];
+        for (int i = height.length - 2; i >= 0; i--) {
+            rightBound[i] = Math.max(rightBound[i + 1], height[i]);
+        }
+        int count = 0;
+        for (int i = 0; i < height.length; i++) {
+            int minBound = Math.min(leftBound[i], rightBound[i]);
+            count += minBound > height[i] ? minBound - height[i] : 0;
+        }
+        return count;
+    }
+
+}
+
+/*
+* 给定 n 个非负整数表示每个宽度为 1 的柱子的高度图，计算按此排列的柱子，下雨之后能接多少雨水。
+
+ 
+
+示例 1：
+
+
+
+输入：height = [0,1,0,2,1,0,1,3,2,1,2,1]
+输出：6
+解释：上面是由数组 [0,1,0,2,1,0,1,3,2,1,2,1] 表示的高度图，在这种情况下，可以接 6 个单位的雨水（蓝色部分表示雨水）。 
+示例 2：
+
+输入：height = [4,2,0,3,2,5]
+输出：9
+ 
+
+提示：
+
+n == height.length
+0 <= n <= 3 * 104
+0 <= height[i] <= 105
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/trapping-rain-water
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+* */
