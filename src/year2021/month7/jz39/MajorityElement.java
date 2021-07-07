b1c1c2be362925f331ead249a3c0898bda977e45
Felix Wang <felix.wang@oocl.com>
2021-07-07 08:41:33 +0800

    JZ.39 - MajorityElement & 摩尔投票法, time is O(n), space is O(1)

---
+++ src/year2021/month7/jz39/MajorityElement.java
@@ -0,0 +1,52 @@
+package year2021.month7.jz39;
+
+public class MajorityElement {
+
+    public static void main(String[] args) {
+        int[] nums1 = {1, 2, 3, 2, 2, 2, 5, 4, 2};
+        System.out.println(majorityElement(nums1));
+    }
+
+    public static int majorityElement(int[] nums) {
+        int candidate = nums[0];
+        int count = 1;
+        for (int i = 1; i < nums.length; i++) {
+            if (count == 0) {
+                candidate = nums[i];
+                count++;
+                continue;
+            }
+            count += candidate == nums[i] ? 1 : -1;
+        }
+        return candidate;
+    }
+
+}
+
+/*
+* 数组中有一个数字出现的次数超过数组长度的一半，请找出这个数字。
+
+ 
+
+你可以假设数组是非空的，并且给定的数组总是存在多数元素。
+
+ 
+
+示例 1:
+
+输入: [1, 2, 3, 2, 2, 2, 5, 4, 2]
+输出: 2
+ 
+
+限制：
+
+1 <= 数组长度 <= 50000
+
+ 
+
+注意：本题与主站 169 题相同：https://leetcode-cn.com/problems/majority-element/
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/shu-zu-zhong-chu-xian-ci-shu-chao-guo-yi-ban-de-shu-zi-lcof
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+* */
