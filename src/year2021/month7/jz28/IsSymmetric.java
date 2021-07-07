bf02a6aa3f3757b3d50073f5f696f2109645c8e7
Felix Wang <felix.wang@oocl.com>
2021-07-07 08:50:41 +0800

    JZ.28 - Recursion, time is O(n), space is O(n)

---
+++ src/year2021/month7/jz28/IsSymmetric.java
@@ -0,0 +1,96 @@
+package year2021.month7.jz28;
+
+public class IsSymmetric {
+
+    public static void main(String[] args) {
+        TreeNode root1 = new TreeNode(1,
+                new TreeNode(2,
+                        new TreeNode(3),
+                        new TreeNode(4)),
+                new TreeNode(2,
+                        new TreeNode(4),
+                        new TreeNode(3)));
+        TreeNode root2 = new TreeNode(1,
+                new TreeNode(2,
+                        null,
+                        new TreeNode(3)),
+                new TreeNode(2,
+                        null,
+                        new TreeNode(3)));
+        System.out.println(isSymmetric(root1));
+        System.out.println(isSymmetric(root2));
+    }
+
+    public static boolean isSymmetric(TreeNode root) {
+        return dfs(root, root);
+    }
+
+    private static boolean dfs(TreeNode left, TreeNode right) {
+        if (left == null && right == null) {
+            return true;
+        }
+        if (left == null || right == null || left.val != right.val) {
+            return false;
+        }
+        return dfs(left.left, right.right) && dfs(left.right, right.left);
+    }
+
+}
+
+class TreeNode {
+
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x) {
+        val = x;
+    }
+
+    public TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+/*
+* 请实现一个函数，用来判断一棵二叉树是不是对称的。如果一棵二叉树和它的镜像一样，那么它是对称的。
+
+例如，二叉树 [1,2,2,3,4,4,3] 是对称的。
+
+    1
+   / \
+  2   2
+ / \ / \
+3  4 4  3
+但是下面这个 [1,2,2,null,3,null,3] 则不是镜像对称的:
+
+    1
+   / \
+  2   2
+   \   \
+   3    3
+
+ 
+
+示例 1：
+
+输入：root = [1,2,2,3,4,4,3]
+输出：true
+示例 2：
+
+输入：root = [1,2,2,null,3,null,3]
+输出：false
+ 
+
+限制：
+
+0 <= 节点个数 <= 1000
+
+注意：本题与主站 101 题相同：https://leetcode-cn.com/problems/symmetric-tree/
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/dui-cheng-de-er-cha-shu-lcof
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+* */
