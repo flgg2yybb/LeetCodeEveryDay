469bbfb0230c9223409417f5679891597a873a33
Felix Wang <felix.wang@oocl.com>
2021-07-09 09:21:52 +0800

    JZ.18 - Iteration, time is O(n), space is O(1)

---
+++ src/year2021/month7/jz18/DeleteNode.java
@@ -0,0 +1,79 @@
+package year2021.month7.jz18;
+
+public class DeleteNode {
+
+    public static void main(String[] args) {
+        ListNode head1 = new ListNode(4, new ListNode(5, new ListNode(1, new ListNode(9))));
+        int val1 = 5;
+        ListNode head2 = new ListNode(4, new ListNode(5, new ListNode(1, new ListNode(9))));
+        int val2 = 1;
+        System.out.println(deleteNode(head1, val1));
+        System.out.println(deleteNode(head2, val2));
+    }
+
+    public static ListNode deleteNode(ListNode head, int val) {
+        ListNode dummy = new ListNode(-1, head);
+        ListNode p = dummy;
+        while (p.next != null) {
+            if (p.next.val == val) {
+                p.next = p.next.next;
+                break;
+            }
+            p = p.next;
+        }
+        return dummy.next;
+    }
+
+}
+
+class ListNode {
+
+    int val;
+    ListNode next;
+
+    ListNode(int x) {
+        val = x;
+    }
+
+    public ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+
+    @Override
+    public String toString() {
+        return "ListNode{" +
+                "val=" + val +
+                ", next=" + next +
+                '}';
+    }
+}
+
+/*
+* 给定单向链表的头指针和一个要删除的节点的值，定义一个函数删除该节点。
+
+返回删除后的链表的头节点。
+
+注意：此题对比原题有改动
+
+示例 1:
+
+输入: head = [4,5,1,9], val = 5
+输出: [4,1,9]
+解释: 给定你链表中值为 5 的第二个节点，那么在调用了你的函数之后，该链表应变为 4 -> 1 -> 9.
+示例 2:
+
+输入: head = [4,5,1,9], val = 1
+输出: [4,5,9]
+解释: 给定你链表中值为 1 的第三个节点，那么在调用了你的函数之后，该链表应变为 4 -> 5 -> 9.
+ 
+
+说明：
+
+题目保证链表中节点的值互不相同
+若使用 C 或 C++ 语言，你不需要 free 或 delete 被删除的节点
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/shan-chu-lian-biao-de-jie-dian-lcof
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+* */
