3f7460909b970a34ec2151bfc9f43335ef9d94b1
Felix Wang <felix.wang@oocl.com>
2021-07-14 08:49:57 +0800

    JZ.55a - DFS, time is O(n), space is O(n)

---
+++ src/year2021/month7/jz55a/MaxDepth.java
@@ -0,0 +1,70 @@
+package year2021.month7.jz55a;
+
+public class MaxDepth {
+
+    public static void main(String[] args) {
+        TreeNode root1 = new TreeNode(1,
+                new TreeNode(2),
+                new TreeNode(3,
+                        new TreeNode(4,
+                                new TreeNode(5),
+                                null),
+                        new TreeNode(6)));
+        TreeNode root2 = null;
+        System.out.println(maxDepth(root1));
+        System.out.println(maxDepth(root2));
+    }
+
+    public static int maxDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int leftDepth = maxDepth(root.left);
+        int rightDepth = maxDepth(root.right);
+        return Math.max(leftDepth, rightDepth) + 1;
+    }
+
+}
+
+class TreeNode {
+
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    public TreeNode(int val) {
+        this.val = val;
+    }
+
+    public TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+/*
+* 输入一棵二叉树的根节点，求该树的深度。从根节点到叶节点依次经过的节点（含根、叶节点）形成树的一条路径，最长路径的长度为树的深度。
+
+例如：
+
+给定二叉树 [3,9,20,null,null,15,7]，
+
+    3
+   / \
+  9  20
+    /  \
+   15   7
+返回它的最大深度 3 。
+
+ 
+
+提示：
+
+节点总数 <= 10000
+注意：本题与主站 104 题相同：https://leetcode-cn.com/problems/maximum-depth-of-binary-tree/
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/er-cha-shu-de-shen-du-lcof
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+* */
