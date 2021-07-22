650b2dd15b60ccfc5b84ddffd53e7ff353624557
Felix Wang <felix.wang@oocl.com>
2021-07-22 19:50:49 +0800

    JZ.14a - DP, time is O(n^2), space is O(n)

--- src/year2021/month7/jz14a/CuttingRope.java
+++ src/year2021/month7/jz14a/CuttingRope.java
@@ -5,8 +5,37 @@ public class CuttingRope {
     public static void main(String[] args) {
         int n1 = 2;
         int n2 = 10;
-        System.out.println(cuttingRope(n1));
-        System.out.println(cuttingRope(n2));
+        System.out.println(cuttingRope1(n1));
+        System.out.println(cuttingRope1(n2));
+    }
+
+    private static int cuttingRope1(int n) {
+        /*
+         * DP, time is O(n^2), space is O(n)
+         * dp[i]：长度为 i 绳子剪成 m 段之后的最大乘积
+         * 思路：
+         * 先看绳子被剪断的第一段，如果只剪掉长度为 1，则没有任何增益，
+         * 因此得从 2 开始剪
+         * 假设第一段剪的长度为 j，剪了一段之后，剩下的 (i - j) 可以减，
+         * 也可以不减
+         *   * 剪的话，最大乘积为 j * dp[i-j]
+         *   * 不剪的话，最大乘积为 j * (i-j)
+         *   * 两者取最大即可
+         * 转移方程：
+         * dp[i] = max{dp[i], j * (i-j), j * dp[i-j]}, j = 2, 3, ... , i - 1
+         * */
+        if (n < 3) {
+            return 1;
+        }
+        int[] dp = new int[n + 1];
+        dp[1] = 1;
+        dp[2] = 1;
+        for (int i = 3; i <= n; i++) {
+            for (int j = 2; j < i; j++) {
+                dp[i] = Math.max(dp[i], Math.max(j * (i - j), j * dp[i - j]));
+            }
+        }
+        return dp[n];
     }
 
     public static int cuttingRope(int n) {
