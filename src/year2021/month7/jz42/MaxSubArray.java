6a2120517b981e477ae9bf0652224ef581124dd5
Felix Wang <felix.wang@oocl.com>
2021-07-26 09:21:57 +0800

    JZ.42 - DP, time is O(n), space is O(n)

---
+++ src/year2021/month7/jz42/MaxSubArray.java
@@ -0,0 +1,63 @@
+package year2021.month7.jz42;
+
+public class MaxSubArray {
+
+    public static void main(String[] args) {
+        int[] nums1 = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
+        int[] nums2 = {1};
+        int[] nums3 = {0};
+        int[] nums4 = {-1};
+        int[] nums5 = {-100000};
+        System.out.println(maxSubArray(nums1));
+        System.out.println(maxSubArray(nums2));
+        System.out.println(maxSubArray(nums3));
+        System.out.println(maxSubArray(nums4));
+        System.out.println(maxSubArray(nums5));
+    }
+
+    public static int maxSubArray(int[] nums) {
+        /*
+         * DP, time is O(n), space is O(n)
+         * 状态定义
+         * dp[i] 表示从 [0,i] 子数组中，其中一个包含下标 i 元素的子数组的最大值
+         * 状态转移方程
+         * dp[i] = max{dp[i-1] + nums[i], nums[i]}
+         * ans = max{dp[i]}, i = 0, 1, ..., n-1
+         * */
+        int n = nums.length;
+        int[] dp = new int[n];
+        dp[0] = nums[0];
+        int max = dp[0];
+        for (int i = 1; i < n; i++) {
+            dp[i] = Math.max(dp[i - 1], 0) + nums[i];
+            max = Math.max(max, dp[i]);
+        }
+        return max;
+    }
+
+}
+
+/*
+* 输入一个整型数组，数组中的一个或连续多个整数组成一个子数组。求所有子数组的和的最大值。
+
+要求时间复杂度为O(n)。
+
+ 
+
+示例1:
+
+输入: nums = [-2,1,-3,4,-1,2,1,-5,4]
+输出: 6
+解释: 连续子数组 [4,-1,2,1] 的和最大，为 6。
+ 
+
+提示：
+
+1 <= arr.length <= 10^5
+-100 <= arr[i] <= 100
+注意：本题与主站 53 题相同：https://leetcode-cn.com/problems/maximum-subarray/
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/lian-xu-zi-shu-zu-de-zui-da-he-lcof
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+* */
