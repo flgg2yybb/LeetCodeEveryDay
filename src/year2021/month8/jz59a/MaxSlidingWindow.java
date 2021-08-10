e2624f8c2447809bddb68759de368eee69cc2c4b
Felix Wang <felix.wang@oocl.com>
2021-08-10 09:12:54 +0800

    JZ.59a - 单调队列, time is O(n), space is O(k)

--- src/year2021/month8/jz59a/MaxSlidingWindow.java
+++ src/year2021/month8/jz59a/MaxSlidingWindow.java
@@ -1,6 +1,8 @@
 package year2021.month8.jz59a;
 
 import java.util.Arrays;
+import java.util.Deque;
+import java.util.LinkedList;
 import java.util.PriorityQueue;
 
 public class MaxSlidingWindow {
@@ -8,7 +10,43 @@ public class MaxSlidingWindow {
     public static void main(String[] args) {
         int[] nums1 = {1, 3, -1, -3, 5, 3, 6, 7};
         int k1 = 3;
-        System.out.println(Arrays.toString(maxSlidingWindow(nums1, k1)));
+        int[] nums2 = {-7, -8, 7, 5, 7, 1, 6, 0};
+        int k2 = 4;
+        System.out.println(Arrays.toString(maxSlidingWindow1(nums1, k1)));
+        System.out.println(Arrays.toString(maxSlidingWindow1(nums2, k2)));
+    }
+
+    private static int[] maxSlidingWindow1(int[] nums, int k) {
+        // 单调不增队列
+        if (nums.length == 0) {
+            return new int[0];
+        }
+        Deque<Integer> queue = new LinkedList<>();
+        for (int i = 0; i < k; i++) {
+            addToQueue(queue, nums[i]);
+        }
+        int[] ans = new int[nums.length - k + 1];
+        ans[0] = queue.peekFirst();
+        int pos = 1;
+        for (int i = k; i < nums.length; i++) {
+            int out = nums[i - k];
+            if (queue.peekFirst() == out) {
+                queue.pollFirst();
+            }
+            addToQueue(queue, nums[i]);
+            ans[pos++] = queue.peekFirst();
+        }
+        return ans;
+    }
+
+    private static void addToQueue(Deque<Integer> queue, int num) {
+        // 此处 queue.peekLast() == num 不能进行出队
+        // 否则对于 [7, 7] 这个数组，其队列元素为 [7]
+        // 则在出队第一个 7 时，就会把队列元素的 7 出掉
+        while (!queue.isEmpty() && queue.peekLast() < num) {
+            queue.pollLast();
+        }
+        queue.offerLast(num);
     }
 
     public static int[] maxSlidingWindow(int[] nums, int k) {
