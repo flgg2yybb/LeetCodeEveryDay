49ddd290d2af24da46219c273cc5ab96ac816809
Felix Wang <felix.wang@oocl.com>
2021-08-17 19:42:35 +0800

    No.1114 - Synchronized(独占锁)

--- src/year2021/month8/no1114/PrintOrderly.java
+++ src/year2021/month8/no1114/PrintOrderly.java
@@ -37,9 +37,50 @@ public class PrintOrderly {
 
 class Foo {
 
-    private volatile int done = 0;
+    private final Object lock = new Object();
+    private boolean firstJobDone = false;
+    private boolean secondJobDone = false;
 
     public Foo() {
+    }
+
+    public void first(Runnable printFirst) throws InterruptedException {
+        synchronized (lock) {
+            // printFirst.run() outputs "first". Do not change or remove this line.
+            printFirst.run();
+            firstJobDone = true;
+            lock.notifyAll();
+        }
+    }
+
+    public void second(Runnable printSecond) throws InterruptedException {
+        synchronized (lock) {
+            while (!firstJobDone) {
+                lock.wait();
+            }
+            // printSecond.run() outputs "second". Do not change or remove this line.
+            printSecond.run();
+            secondJobDone = true;
+            lock.notifyAll();
+        }
+    }
+
+    public void third(Runnable printThird) throws InterruptedException {
+        synchronized (lock) {
+            while (!secondJobDone) {
+                lock.wait();
+            }
+            // printThird.run() outputs "third". Do not change or remove this line.
+            printThird.run();
+        }
+    }
+}
+
+class Foo3 {
+
+    private volatile int done = 0;
+
+    public Foo3() {
     }
 
     public void first(Runnable printFirst) throws InterruptedException {
