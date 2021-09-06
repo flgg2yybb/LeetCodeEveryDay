c8dc3def3e2cf41c8e33dcd1bb0a385a72fb936c
Felix <felix.wang@oocl.com>
2021-09-06 09:09:43 +0800

    No.322 - DP, time is O(nc), space is O(n), n is the amount and c is the length of coins

---
+++ src/year2021/month9/no322/CoinChange.java
@@ -0,0 +1,81 @@
+package year2021.month9.no322;
+
+import java.util.Arrays;
+
+public class CoinChange {
+    public static void main(String[] args) {
+        int[] coins1 = {1, 2, 5};
+        int amount1 = 11;
+        int[] coins2 = {2};
+        int amount2 = 3;
+        int[] coins3 = {1};
+        int amount3 = 0;
+        int[] coins4 = {1};
+        int amount4 = 1;
+        int[] coins5 = {1};
+        int amount5 = 2;
+        System.out.println(coinChange(coins1, amount1));
+        System.out.println(coinChange(coins2, amount2));
+        System.out.println(coinChange(coins3, amount3));
+        System.out.println(coinChange(coins4, amount4));
+        System.out.println(coinChange(coins5, amount5));
+    }
+
+    public static int coinChange(int[] coins, int amount) {
+        final int MAX = amount + 1;
+        int[] dp = new int[amount + 1];
+        Arrays.fill(dp, MAX);
+        dp[0] = 0;
+        for (int i = 1; i <= amount; i++) {
+            for (int coin : coins) {
+                if (i - coin >= 0) {
+                    dp[i] = Math.min(dp[i], dp[i - coin] + 1);
+                }
+            }
+        }
+        return dp[amount] == MAX ? -1 : dp[amount];
+    }
+}
+
+/*
+* 给你一个整数数组 coins ，表示不同面额的硬币；以及一个整数 amount ，表示总金额。
+
+计算并返回可以凑成总金额所需的 最少的硬币个数 。如果没有任何一种硬币组合能组成总金额，返回 -1 。
+
+你可以认为每种硬币的数量是无限的。
+
+ 
+
+示例 1：
+
+输入：coins = [1, 2, 5], amount = 11
+输出：3 
+解释：11 = 5 + 5 + 1
+示例 2：
+
+输入：coins = [2], amount = 3
+输出：-1
+示例 3：
+
+输入：coins = [1], amount = 0
+输出：0
+示例 4：
+
+输入：coins = [1], amount = 1
+输出：1
+示例 5：
+
+输入：coins = [1], amount = 2
+输出：2
+ 
+
+提示：
+
+1 <= coins.length <= 12
+1 <= coins[i] <= 231 - 1
+0 <= amount <= 104
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/coin-change
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+* */
