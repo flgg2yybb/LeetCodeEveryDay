d5083cd0f6b7859a43bf1f11a8efce04e55a4c70
Felix <felix.wang@oocl.com>
2021-09-08 09:10:30 +0800

    No.329 - DP & PriorityQueue, time is O(mn logmn), space is O(mn)

---
+++ src/year2021/month9/no329/LongestIncreasingPathInMatrix.java
@@ -0,0 +1,115 @@
+package year2021.month9.no329;
+
+import java.util.PriorityQueue;
+
+public class LongestIncreasingPathInMatrix {
+    public static void main(String[] args) {
+        int[][] matrix1 = {
+                {9, 9, 4},
+                {6, 6, 8},
+                {2, 1, 1}
+        };
+        int[][] matrix2 = {
+                {3, 4, 5},
+                {3, 2, 6},
+                {2, 2, 1}
+        };
+        int[][] matrix3 = {{1}};
+        System.out.println(longestIncreasingPath(matrix1));
+        System.out.println(longestIncreasingPath(matrix2));
+        System.out.println(longestIncreasingPath(matrix3));
+    }
+
+    public static int longestIncreasingPath(int[][] matrix) {
+        /*
+        * DP, time is O(mn * logmn), space is O(mn)
+        * 状态定义：
+        * dp[i][j] 表示以当前元素(i,j)为起点的最长递增路径的长度
+        * 状态转移方程：
+        * dp[i][j] = 1 + max{dp[x][y]},
+        * (x,y) 为 (i,j) 四联通方向的相邻坐标，且有 matrix[x][y] > matrix[i][j]
+        * 可见，状态转移由较大的元素，转向较小的元素，因此先遍历较大元素，故使用优先队列
+        * */
+        int m = matrix.length;
+        int n = matrix[0].length;
+        PriorityQueue<Element> queue = new PriorityQueue<>((c1, c2) -> c2.value - c1.value);
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                queue.offer(new Element(i, j, matrix[i][j]));
+            }
+        }
+        int[][] dp = new int[m][n];
+        int[][] directions = {{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
+        int longest = 0;
+        while (!queue.isEmpty()) {
+            Element element = queue.poll();
+            int currentStep = 1;
+            int nextSetp = 0;
+            for (int[] direction : directions) {
+                int nextX = element.x + direction[0];
+                int nextY = element.y + direction[1];
+                if (isLegalAccess(m, n, nextX, nextY) && matrix[nextX][nextY] > element.value) {
+                    nextSetp = Math.max(nextSetp, dp[nextX][nextY]);
+                }
+            }
+            int totalStep = currentStep + nextSetp;
+            dp[element.x][element.y] = totalStep;
+            longest = Math.max(longest, totalStep);
+        }
+        return longest;
+    }
+
+    private static boolean isLegalAccess(int m, int n, int x, int y) {
+        return x >= 0 && x < m && y >= 0 && y < n;
+    }
+}
+
+class Element {
+
+    final int x;
+    final int y;
+    final int value;
+
+    public Element(int x, int y, int value) {
+        this.x = x;
+        this.y = y;
+        this.value = value;
+    }
+}
+
+/*
+* 给定一个 m x n 整数矩阵 matrix ，找出其中 最长递增路径 的长度。
+
+对于每个单元格，你可以往上，下，左，右四个方向移动。 你 不能 在 对角线 方向上移动或移动到 边界外（即不允许环绕）。
+
+ 
+
+示例 1：
+
+
+输入：matrix = [[9,9,4],[6,6,8],[2,1,1]]
+输出：4 
+解释：最长递增路径为 [1, 2, 6, 9]。
+示例 2：
+
+
+输入：matrix = [[3,4,5],[3,2,6],[2,2,1]]
+输出：4 
+解释：最长递增路径是 [3, 4, 5, 6]。注意不允许在对角线方向上移动。
+示例 3：
+
+输入：matrix = [[1]]
+输出：1
+ 
+
+提示：
+
+m == matrix.length
+n == matrix[i].length
+1 <= m, n <= 200
+0 <= matrix[i][j] <= 231 - 1
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/longest-increasing-path-in-a-matrix
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+* */
