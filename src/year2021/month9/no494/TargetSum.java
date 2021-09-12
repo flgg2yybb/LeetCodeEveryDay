db96e859aab0cc453ecceca38b5cb6007c26c300
Felix <felix.wang@oocl.com>
2021-09-12 11:15:54 +0800

    No.494 - DP, time is O(nc), space is O(nc), c is constant and equals 1000

--- src/year2021/month9/no494/TargetSum.java
+++ src/year2021/month9/no494/TargetSum.java
@@ -1,18 +1,45 @@
 package year2021.month9.no494;
 
 public class TargetSum {
+    private static int count = 0;
+
     public static void main(String[] args) {
         int[] nums1 = {1, 1, 1, 1, 1};
         int target1 = 3;
         int[] nums2 = {1};
-        int target2 = 1;
+        int target2 = 2;
+        int[] nums3 = {1, 999};
+        int target3 = 998;
         System.out.println(findTargetSumWays(nums1, target1));
         System.out.println(findTargetSumWays(nums2, target2));
+        System.out.println(findTargetSumWays(nums3, target3));
     }
 
-    private static int count = 0;
+    private static int findTargetSumWays(int[] nums, int target) {
+        /*
+         * DP, time is O(n), space is O(n)
+         * 状态定义
+         * dp[i][j] 表示数组 [0,i] 上元素排列为和为 j 的个数
+         * 状态转移方程
+         * dp[i][j] = dp[i-1][j-nums[i]] + dp[i-1][j+nums[i]]
+         * 由于 j 会有负数，则需要做偏移，由题可知，偏移量为 1000
+         * */
+        final int offset = 1000;
+        int[][] dp = new int[nums.length][2 * offset + 1];
+        dp[0][offset + nums[0]] += 1;
+        dp[0][offset - nums[0]] += 1;
+        for (int i = 1; i < nums.length; i++) {
+            int num = Math.abs(nums[i]);
+            for (int j = 0; j <= 2 * offset; j++) {
+                int left = j - num >= 0 ? dp[i - 1][j - num] : 0;
+                int right = j + num <= 2 * offset ? dp[i - 1][j + num] : 0;
+                dp[i][j] = left + right;
+            }
+        }
+        return dp[nums.length - 1][offset + target];
+    }
 
-    public static int findTargetSumWays(int[] nums, int target) {
+    public static int findTargetSumWays1(int[] nums, int target) {
         // backtrack
         count = 0;
         dfs(nums, target, 0, 0);
