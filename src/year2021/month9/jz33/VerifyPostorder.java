5e968993a2b14746d63acde0cc027b3697cd8c94
Felix <felix.wang@oocl.com>
2021-09-14 09:08:10 +0800

    JZ.33 - DFS, time is O(n^2), space is O(n)

---
+++ src/year2021/month9/jz33/VerifyPostorder.java
@@ -0,0 +1,67 @@
+package year2021.month9.jz33;
+
+public class VerifyPostorder {
+    public static void main(String[] args) {
+        int[] postorder1 = {1, 6, 3, 2, 5};
+        int[] postorder2 = {1, 3, 2, 6, 5};
+        System.out.println(verifyPostorder(postorder1));
+        System.out.println(verifyPostorder(postorder2));
+    }
+
+    public static boolean verifyPostorder(int[] postorder) {
+        /*
+         * 后序遍历，左 -> 右 -> 跟
+         * 二叉搜索树：
+         *   * 左子树所有节点值 < 根节点值
+         *   * 右子树所有节点值 > 根节点值
+         * */
+        return dfs(postorder, 0, postorder.length - 1);
+    }
+
+    private static boolean dfs(int[] postorder, int start, int end) {
+        if (start >= end) {
+            return true;
+        }
+        // end 为 根节点
+        int left = start;
+        while (postorder[left] < postorder[end]) {
+            left++;
+        }
+        int right = left;
+        while (postorder[right] > postorder[end]) {
+            right++;
+        }
+        return right == end && dfs(postorder, start, left - 1) && dfs(postorder, left, right - 1);
+    }
+}
+
+/*
+* 输入一个整数数组，判断该数组是不是某二叉搜索树的后序遍历结果。如果是则返回 true，否则返回 false。假设输入的数组的任意两个数字都互不相同。
+
+ 
+
+参考以下这颗二叉搜索树：
+
+     5
+    / \
+   2   6
+  / \
+ 1   3
+示例 1：
+
+输入: [1,6,3,2,5]
+输出: false
+示例 2：
+
+输入: [1,3,2,6,5]
+输出: true
+ 
+
+提示：
+
+数组长度 <= 1000
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/er-cha-sou-suo-shu-de-hou-xu-bian-li-xu-lie-lcof
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+* */
