9be972462cd035c4421f686a3d422ac1494b6f0d
Felix <742863722@qq.com>
2021-10-03 15:00:21 +0800

    No.78 - Backtrack, time is O(n*2^n), space is O(2^n)

---
+++ src/year2021/month10/no78/Subsets.java
@@ -0,0 +1,59 @@
+package year2021.month10.no78;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Subsets {
+    public static void main(String[] args) {
+        int[] nums1 = {1, 2, 3};
+        int[] nums2 = {0};
+        System.out.println(subsets(nums1));
+        System.out.println(subsets(nums2));
+    }
+
+    public static List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> ans = new ArrayList<>();
+        List<Integer> list = new ArrayList<>();
+        backtrack(nums, ans, list, 0);
+        return ans;
+    }
+
+    private static void backtrack(int[] nums, List<List<Integer>> ans, List<Integer> list, int pos) {
+        if (pos == nums.length) {
+            ans.add(new ArrayList<>(list));
+            return;
+        }
+        backtrack(nums, ans, list, pos + 1);
+        list.add(nums[pos]);
+        backtrack(nums, ans, list, pos + 1);
+        list.remove(list.size() - 1);
+    }
+}
+
+/*
+ * 给你一个整数数组 nums ，数组中的元素 互不相同 。返回该数组所有可能的子集（幂集）。
+
+解集 不能 包含重复的子集。你可以按 任意顺序 返回解集。
+
+ 
+
+示例 1：
+
+输入：nums = [1,2,3]
+输出：[[],[1],[2],[1,2],[3],[1,3],[2,3],[1,2,3]]
+示例 2：
+
+输入：nums = [0]
+输出：[[],[0]]
+ 
+
+提示：
+
+1 <= nums.length <= 10
+-10 <= nums[i] <= 10
+nums 中的所有元素 互不相同
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/subsets
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+* */
