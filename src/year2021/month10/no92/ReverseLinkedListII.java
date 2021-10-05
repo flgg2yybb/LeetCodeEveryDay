10e4280a87bf3b436d66ab2b26304aa1ae813f4a
Felix <742863722@qq.com>
2021-10-05 10:54:01 +0800

    No.92 - Iteration & Refactor, time is O(n), space is O(1)

--- src/year2021/month10/no92/ReverseLinkedListII.java
+++ src/year2021/month10/no92/ReverseLinkedListII.java
@@ -8,8 +8,26 @@ public class ReverseLinkedListII {
         int left1 = 2, right1 = 4;
         ListNode head2 = new ListNode(1);
         int left2 = 1, right2 = 1;
-        System.out.println(reverseBetween1(head1, left1, right1));
-        System.out.println(reverseBetween1(head2, left2, right2));
+        System.out.println(reverseBetween2(head1, left1, right1));
+        System.out.println(reverseBetween2(head2, left2, right2));
+    }
+
+    private static ListNode reverseBetween2(ListNode head, int left, int right) {
+        // clean code
+        ListNode root = new ListNode(-1, head);
+        ListNode pre = root;
+        for (int i = 0; i < left - 1; i++) {
+            pre = pre.next;
+        }
+        ListNode cur = pre.next;
+        for (int i = 0; i < right - left; i++) {
+            // 头插法
+            ListNode next = cur.next;
+            cur.next = cur.next.next;
+            next.next = pre.next;
+            pre.next = next;
+        }
+        return root.next;
     }
 
     private static ListNode reverseBetween1(ListNode head, int left, int right) {
