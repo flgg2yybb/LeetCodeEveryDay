a1e759ab04bf4bf497bd360e1a15aa13cb2b5371
Felix Wang <felix.wang@oocl.com>
2021-10-10 15:31:50 +0800

    No.226 - DFS, time is O(n), space is O(n)

---
+++ src/year2021/month10/no226/InvertBinaryTree.java
@@ -0,0 +1,87 @@
+package year2021.month10.no226;
+
+public class InvertBinaryTree {
+
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(4,
+                new TreeNode(2,
+                        new TreeNode(1),
+                        new TreeNode(3)),
+                new TreeNode(7,
+                        new TreeNode(6),
+                        new TreeNode(9)));
+        TreeNode treeNode = invertTree(root);
+        dfs(treeNode);
+    }
+
+    public static TreeNode invertTree(TreeNode root) {
+        if (root == null) {
+            return root;
+        }
+        TreeNode left = root.left;
+        TreeNode right = root.right;
+        root.left = right;
+        root.right = left;
+        invertTree(root.left);
+        invertTree(root.right);
+        return root;
+    }
+
+    private static void dfs(TreeNode treeNode) {
+        if (treeNode != null) {
+            System.out.print(treeNode.val + " ");
+            dfs(treeNode.left);
+            dfs(treeNode.right);
+        }
+    }
+
+}
+
+class TreeNode {
+
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+/*
+* 翻转一棵二叉树。
+
+示例：
+
+输入：
+
+     4
+   /   \
+  2     7
+ / \   / \
+1   3 6   9
+输出：
+
+     4
+   /   \
+  7     2
+ / \   / \
+9   6 3   1
+备注:
+这个问题是受到 Max Howell 的 原问题 启发的 ：
+
+谷歌：我们90％的工程师使用您编写的软件(Homebrew)，但是您却无法在面试时在白板上写出翻转二叉树这道题，这太糟糕了。
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/invert-binary-tree
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+* */
