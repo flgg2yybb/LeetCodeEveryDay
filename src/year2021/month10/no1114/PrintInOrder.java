8d71509179c30d62b5e35893a89220cb8e251973
Felix <742863722@qq.com>
2021-10-22 23:15:53 +0800

    No.1114 - 按序打印, CountDownLatch

--- src/year2021/month10/no1114/PrintInOrder.java
+++ src/year2021/month10/no1114/PrintInOrder.java
@@ -3,6 +3,7 @@ package year2021.month10.no1114;
 
 import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.Semaphore;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.locks.Condition;
@@ -42,10 +43,39 @@ public class PrintInOrder {
 
 class Foo {
 
+    private final CountDownLatch secondLatch = new CountDownLatch(1);
+    private final CountDownLatch thirdLatch = new CountDownLatch(1);
+
+    public Foo() {
+
+    }
+
+    public void first(Runnable printFirst) throws InterruptedException {
+        // printFirst.run() outputs "first". Do not change or remove this line.
+        printFirst.run();
+        secondLatch.countDown();
+    }
+
+    public void second(Runnable printSecond) throws InterruptedException {
+        secondLatch.await();
+        // printSecond.run() outputs "second". Do not change or remove this line.
+        printSecond.run();
+        thirdLatch.countDown();
+    }
+
+    public void third(Runnable printThird) throws InterruptedException {
+        thirdLatch.await();
+        // printThird.run() outputs "third". Do not change or remove this line.
+        printThird.run();
+    }
+}
+
+class Foo5 {
+
     private final BlockingQueue<Integer> blockingQueue2 = new ArrayBlockingQueue<>(1);
     private final BlockingQueue<Integer> blockingQueue3 = new ArrayBlockingQueue<>(1);
 
-    public Foo() {
+    public Foo5() {
 
     }
 
