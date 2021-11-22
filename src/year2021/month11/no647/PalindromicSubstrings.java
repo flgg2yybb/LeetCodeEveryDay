80b7ca5270975c53321e21932a96beeb174c2c74
Felix <742863722@qq.com>
2021-11-22 16:58:18 +0800

    No.647 - DP, time is O(n^2), space is O(n^2)

---
+++ src/year2021/month11/no647/PalindromicSubstrings.java
@@ -0,0 +1,68 @@
+package year2021.month11.no647;
+
+public class PalindromicSubstrings {
+    public static void main(String[] args) {
+        String s1 = "abc";
+        String s2 = "aaa";
+        System.out.println(countSubstrings(s1));
+        System.out.println(countSubstrings(s2));
+    }
+
+    public static int countSubstrings(String s) {
+        // dp[i][j] = s[i] == s[j] && dp[i+1][j-1]
+        int count = 0;
+        boolean[][] dp = new boolean[s.length()][s.length()];
+        for (int i = 0; i < s.length(); i++) {
+            dp[i][i] = true;
+        }
+        count += s.length();
+        for (int i = 0; i < s.length() - 1; i++) {
+            if (s.charAt(i) == s.charAt(i + 1)) {
+                dp[i][i + 1] = true;
+                count++;
+            }
+        }
+        for (int i = s.length() - 3; i >= 0; i--) {
+            for (int j = i + 2; j < s.length(); j++) {
+                if (s.charAt(i) == s.charAt(j) && dp[i + 1][j - 1]) {
+                    dp[i][j] = true;
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+}
+
+/*
+ * 给你一个字符串 s ，请你统计并返回这个字符串中 回文子串 的数目。
+
+回文字符串 是正着读和倒过来读一样的字符串。
+
+子字符串 是字符串中的由连续字符组成的一个序列。
+
+具有不同开始位置或结束位置的子串，即使是由相同的字符组成，也会被视作不同的子串。
+
+ 
+
+示例 1：
+
+输入：s = "abc"
+输出：3
+解释：三个回文子串: "a", "b", "c"
+示例 2：
+
+输入：s = "aaa"
+输出：6
+解释：6个回文子串: "a", "a", "a", "aa", "aa", "aaa"
+ 
+
+提示：
+
+1 <= s.length <= 1000
+s 由小写英文字母组成
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/palindromic-substrings
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+* */
