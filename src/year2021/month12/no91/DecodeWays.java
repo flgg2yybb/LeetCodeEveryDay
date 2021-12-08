a706699967eff63e853b0da53781719fd0d98532
Felix Wang <felix.wang@oocl.com>
2021-12-08 17:09:57 +0800

    No.91 - DP, time is O(n), space is O(n)

---
+++ src/year2021/month12/no91/DecodeWays.java
@@ -0,0 +1,98 @@
+package year2021.month12.no91;
+
+public class DecodeWays {
+
+    public static void main(String[] args) {
+        String s1 = "12";
+        String s2 = "226";
+        String s3 = "0";
+        String s4 = "06";
+        String s5 = "106";
+        String s6 = "006";
+        System.out.println(numDecodings(s1));
+        System.out.println(numDecodings(s2));
+        System.out.println(numDecodings(s3));
+        System.out.println(numDecodings(s4));
+        System.out.println(numDecodings(s5));
+        System.out.println(numDecodings(s6));
+    }
+
+    public static int numDecodings(String s) {
+        /*
+         * DP
+         * 状态定义：
+         * dp[i] 表示 s[0,i) 所能转换成不同字母的数量
+         * 状态转移方程：
+         * if (s[i] != '0')
+         *   dp[i] = dp[i-1]
+         * if s[i-1] != '0' && (s[i-1] - '0') * 10 + (s[i] - '0') <= 26
+         *   dp[i] += dp[i-2]
+         * */
+        int n = s.length();
+        int[] dp = new int[n + 1];
+        dp[0] = 1;
+        for (int i = 1; i <= n; i++) {
+            if (s.charAt(i - 1) != '0') {
+                dp[i] = dp[i - 1];
+            }
+            if (i > 1 && s.charAt(i - 2) != '0' && ((s.charAt(i - 2) - '0') * 10 + (s.charAt(i - 1) - '0') <= 26)) {
+                dp[i] += dp[i - 2];
+            }
+        }
+        return dp[n];
+    }
+
+}
+
+/*
+* 一条包含字母 A-Z 的消息通过以下映射进行了 编码 ：
+
+'A' -> 1
+'B' -> 2
+...
+'Z' -> 26
+要 解码 已编码的消息，所有数字必须基于上述映射的方法，反向映射回字母（可能有多种方法）。例如，"11106" 可以映射为：
+
+"AAJF" ，将消息分组为 (1 1 10 6)
+"KJF" ，将消息分组为 (11 10 6)
+注意，消息不能分组为  (1 11 06) ，因为 "06" 不能映射为 "F" ，这是由于 "6" 和 "06" 在映射中并不等价。
+
+给你一个只含数字的 非空 字符串 s ，请计算并返回 解码 方法的 总数 。
+
+题目数据保证答案肯定是一个 32 位 的整数。
+
+ 
+
+示例 1：
+
+输入：s = "12"
+输出：2
+解释：它可以解码为 "AB"（1 2）或者 "L"（12）。
+示例 2：
+
+输入：s = "226"
+输出：3
+解释：它可以解码为 "BZ" (2 26), "VF" (22 6), 或者 "BBF" (2 2 6) 。
+示例 3：
+
+输入：s = "0"
+输出：0
+解释：没有字符映射到以 0 开头的数字。
+含有 0 的有效映射是 'J' -> "10" 和 'T'-> "20" 。
+由于没有字符，因此没有有效的方法对此进行解码，因为所有数字都需要映射。
+示例 4：
+
+输入：s = "06"
+输出：0
+解释："06" 不能映射到 "F" ，因为字符串含有前导 0（"6" 和 "06" 在映射中并不等价）。
+ 
+
+提示：
+
+1 <= s.length <= 100
+s 只包含数字，并且可能包含前导零。
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/decode-ways
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+* */
