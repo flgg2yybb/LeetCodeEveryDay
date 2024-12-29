450641ecb6ab0dd454c3f6101d351fa11dd6c512
ZHUANG Yong <yongz0714@163.com>
2024-12-29 14:25:34 +0800

    No.15 - Two Pointers, time: O(n^2), space: O(n)

--- go/year2024/month12/no15/ThreeSum.go
+++ go/year2024/month12/no15/ThreeSum.go
@@ -5,8 +5,43 @@ import (
 	"sort"
 )
 
-// 【Timeout】Two Pointers, time: O(n^2), space: O(n)
+// Two Pointers, time: O(n^2), space: O(n)
 func threeSum(nums []int) [][]int {
+	result := make([][]int, 0)
+	// sort and deduplicate when search all result set
+	sort.Slice(nums, func(i, j int) bool { return nums[i] < nums[j] })
+	for i := 0; i < len(nums)-2; i++ {
+		if i > 0 && nums[i] == nums[i-1] {
+			continue
+		}
+		// array is sorted and use the monotony to control the pointer moving
+		// Note: cannot use the same solution of two sums that will contain a duplicated result
+		left := i + 1
+		right := len(nums) - 1
+		for left < right {
+			sum := nums[i] + nums[left] + nums[right]
+			if sum == 0 {
+				result = append(result, []int{nums[i], nums[left], nums[right]})
+			}
+			if sum > 0 {
+				right--
+				for left < right && nums[right] == nums[right+1] {
+					right--
+				}
+			} else { // sum <= 0
+				left++
+				for left < right && nums[left] == nums[left-1] {
+					left++
+				}
+			}
+
+		}
+	}
+	return result
+}
+
+// 【Timeout】Two Pointers, time: O(n^2), space: O(n)
+func threeSum1(nums []int) [][]int {
 	ans := make([][]int, 0)
 	// Two Sum, get all results (might contain duplicated results)
 	for i := 0; i < len(nums)-2; i++ {
