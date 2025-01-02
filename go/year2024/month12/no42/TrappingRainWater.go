b4a5e4835c9d003640bc80a86b003c75fc205a26
ZHUANG Yong <yongz0714@163.com>
2025-01-02 12:05:57 +0800

    No.12 - Monotone stack , time: O(n), space: O(1)

--- go/year2024/month12/no42/TrappingRainWater.go
+++ go/year2024/month12/no42/TrappingRainWater.go
@@ -2,12 +2,66 @@ package main
 
 import "math"
 
+type Stack[T any] struct {
+	elements []T
+}
+
+func NewStack[T any]() *Stack[T] {
+	return &Stack[T]{elements: make([]T, 0)}
+}
+
+func (s *Stack[T]) Push(e T) {
+	s.elements = append(s.elements, e)
+}
+
+func (s *Stack[T]) Pop() T {
+	e := s.elements[len(s.elements)-1]
+	s.elements = s.elements[:len(s.elements)-1]
+	return e
+}
+
+func (s *Stack[T]) Peek() T {
+	return s.elements[len(s.elements)-1]
+}
+
+func (s *Stack[T]) IsEmpty() bool {
+	return len(s.elements) == 0
+}
+
+// Monotone Stack, time: O(n), space: O(n)
+// 思路：维护一个单调递减栈，从左到右遍历各个点，存储每个点的索引
+// 当某个点 A 的高度，大于栈顶元素对应的高度时 [height[stack.peek()] < height[A]]
+// 即说明可能存在一个凹槽可以积累雨水
+// 则弹出栈顶元素 B，假设弹出后，新的栈顶为 C
+// 如果 height[C] > height[B]，则说明有凹槽
+// 积累的雨水量为：(min(height[C], height[A]) - height[B]) * (A - C - 1)
+// 持续弹出栈顶，直到栈顶元素不小于当前元素 A ，继续往右遍历，循环往复
+func trap(height []int) int {
+	sum := 0
+	stack := NewStack[int]()
+	for A := 0; A < len(height); A++ {
+		for !stack.IsEmpty() && height[stack.Peek()] < height[A] {
+			B := stack.Pop()
+			if !stack.IsEmpty() {
+				C := stack.Peek()
+				// 此处 height[A], height[C] > height[B]，因此如果 B 和 C 同高，则 high = 0，相当于不积累雨水量
+				high := int(math.Min(float64(height[C]), float64(height[A]))) - height[B]
+				width := A - C - 1
+				sum += high * width
+			}
+		}
+		stack.Push(A)
+
+	}
+	return sum
+}
+
 // Two Pointers, time: O(n), space: O(1)
 // 思路：如果可以知道每一个点左右两边的最长板，即可知道当前点可以积累多少雨水
 // 因此可以使用双指针，分别从左右两边向中间靠拢，维护当前区间内的左右两边的最长板
 // 又因为雨水量只和较短的长版有关，所以仅需要维护左右两边的最长板中的短版即可
 // 每次移动双指针较短的那一边，逐步积累每一个点的雨水，即可得到答案
-func trap(height []int) int {
+func trap1(height []int) int {
 	sum := 0
 	left := 0
 	right := len(height) - 1
