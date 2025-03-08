01d22a99963a5f1f1976ad056797d9cd006134a8
ZHUANG Yong <yongz0714@163.com>
2025-03-08 09:45:58 +0800

    No.21 - Merge Sorted LinkedList, time: O(nm), space: O(1)

---
+++ java/year2025/month3/no21/MergeTwoSortedLists.java
@@ -0,0 +1,56 @@
+package year2025.month3.no21;
+
+public class MergeTwoSortedLists {
+    public static void main(String[] args) {
+        ListNode list1 = new ListNode(1, new ListNode(2, new ListNode(4)));
+        ListNode list2 = new ListNode(1, new ListNode(3, new ListNode(4)));
+        System.out.println(mergeTwoLists(list1, list2));
+        ListNode list3 = new ListNode(1, new ListNode(2, new ListNode(4)));
+        ListNode list4 = null;
+        System.out.println(mergeTwoLists(list3, list4));
+    }
+
+    // Merge Sorted Lists, time is O(n+m), space is O(1)
+    public static ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        ListNode head = new ListNode(-1);
+        ListNode cur = head;
+        while (list1 != null && list2 != null) {
+            if (list1.val <= list2.val) {
+                cur.next = list1;
+                list1 = list1.next;
+            } else {
+                cur.next = list2;
+                list2 = list2.next;
+            }
+            cur = cur.next;
+        }
+        if (list1 != null) {
+            cur.next = list1;
+        } else {
+            cur.next = list2;
+        }
+        return head.next;
+    }
+}
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+
+    @Override
+    public String toString() {
+        return "ListNode{" + "val=" + val + ", next=" + next + '}';
+    }
+}
