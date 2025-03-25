3c7c402eab3468fabf92cbe81e95bb925acfbf4e
Feilong Wang <feilong.wang@shopee.com>
2025-03-25 15:37:33 +0800

    No.94 - In-Order Traversal, times: O(n), space: O(n)

--- go/year2025/month3/no94/BinaryTreeInorderTraversal.go
+++ go/year2025/month3/no94/BinaryTreeInorderTraversal.go
@@ -20,17 +20,36 @@ type TreeNode struct {
 // Iteration, times: O(n), space: O(n)
 func inorderTraversal(root *TreeNode) []int {
 	ans := make([]int, 0)
-	pre := root
+	p := root
 	stack := make([]*TreeNode, 0)
-	for pre != nil || len(stack) > 0 {
-		if pre != nil { // 一直往左走
-			stack = append(stack, pre)
-			pre = pre.Left
+	for len(stack) > 0 || p != nil {
+		for p != nil {
+			stack = append(stack, p)
+			p = p.Left // 左
+		}
+		pop := stack[len(stack)-1]
+		stack = stack[0 : len(stack)-1]
+		ans = append(ans, pop.Val) // 根
+		p = pop.Right              // 右
+
+	}
+	return ans
+}
+
+// Iteration, times: O(n), space: O(n)
+func inorderTraversal2(root *TreeNode) []int {
+	ans := make([]int, 0)
+	p := root
+	stack := make([]*TreeNode, 0)
+	for p != nil || len(stack) > 0 {
+		if p != nil { // 一直往左走
+			stack = append(stack, p)
+			p = p.Left
 		} else { // 没有左节点了，栈顶元素为上一个节点，弹出栈顶元素，读取栈顶元素值，然后遍历栈顶元素的右节点，继续往作左走
 			pop := stack[len(stack)-1]
 			stack = stack[0 : len(stack)-1]
 			ans = append(ans, pop.Val)
-			pre = pop.Right
+			p = pop.Right
 		}
 	}
 	return ans
