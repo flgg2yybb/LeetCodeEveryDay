dc29c0cb24325efd9ab7a2d2bb846a0b6ac00269
Feilong Wang <feilong.wang@shopee.com>
2025-04-15 10:54:52 +0800

    No.347 - Partition, times: O(n), space: O(n)

--- go/year2025/month4/no347/TopKFrequentElements.go
+++ go/year2025/month4/no347/TopKFrequentElements.go
@@ -3,6 +3,7 @@ package main
 import (
 	"container/heap"
 	"fmt"
+	rand2 "math/rand/v2"
 )
 
 func main() {
@@ -15,6 +16,68 @@ func main() {
 	nums3 := []int{1, 2}
 	k3 := 2
 	println(fmt.Sprintf("%v", topKFrequent(nums3, k3))) // [1,2]
+}
+
+// Partition，times: O(n)，space: O(n)
+func topKFrequent(nums []int, k int) []int {
+	num2FreqMap := make(map[int]int)
+	for _, num := range nums {
+		num2FreqMap[num]++
+	}
+	freqs := make([]int, 0, len(num2FreqMap))
+	for _, freq := range num2FreqMap {
+		freqs = append(freqs, freq)
+	}
+	threshold := getKthMaxValue(freqs, k)
+	ans := make([]int, 0, k)
+	for num, freq := range num2FreqMap {
+		if freq >= threshold {
+			ans = append(ans, num)
+		}
+	}
+	return ans
+}
+
+// 平均情况下，时间复杂度为 O(N)，最坏时间复杂度：O(N^2)
+// 设处理长度为 N 的数组的时间复杂度为 f(N)。由于处理的过程包括一次遍历和一次子分支的递归
+//   - 最好情况下，有 f(N)=O(N)+f(N/2)，根据 主定理，能够得到 f(N)=O(N)。
+//   - 最坏情况下，每次取的中枢数组的元素都位于数组的两端，时间复杂度退化为 O(N^2 )。但由于每次递归会先随机选取中枢元素，故出现最坏情况的概率很低
+//   - 平均情况下，时间复杂度为 O(N)
+func getKthMaxValue(freqs []int, k int) int {
+	start, end := 0, len(freqs)-1
+	if k-1 < start || k-1 > end {
+		panic("k out of range")
+	}
+	for start < end {
+		mid := partition(freqs, start, end)
+		if mid == k-1 {
+			return freqs[mid]
+		} else if mid > k-1 {
+			end = mid - 1
+		} else { // mid < k-1
+			start = mid + 1
+		}
+	}
+	return freqs[start]
+}
+
+func partition(freqs []int, start int, end int) int {
+	rand := rand2.IntN(end-start+1) + start
+	freqs[start], freqs[rand] = freqs[rand], freqs[start]
+	pivot := freqs[start]
+	left, right := start, end
+	for left < right { // 从大到小排序
+		for left < right && freqs[right] <= pivot {
+			right--
+		}
+		freqs[left] = freqs[right] // 此处 right 不用赋值，最终找到 pivot index 后会赋值上 pivot
+		for left < right && freqs[left] >= pivot {
+			left++
+		}
+		freqs[right] = freqs[left]
+	}
+	freqs[left] = pivot
+	return left
 }
 
 type MinHeap []entry
@@ -48,7 +111,7 @@ type entry struct {
 }
 
 // 最小堆，times: O(nlogk)，space: O(n)
-func topKFrequent(nums []int, k int) []int {
+func topKFrequent1(nums []int, k int) []int {
 	num2FreqMap := make(map[int]int)
 	for _, num := range nums {
 		num2FreqMap[num]++
